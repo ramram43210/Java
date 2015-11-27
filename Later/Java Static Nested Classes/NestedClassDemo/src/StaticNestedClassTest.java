a78008375f8032e6e8fca5e3d673de47685ecb4a
Ramesh <Ramesh.Babu@neustar.biz>
2015-11-27 13:27:34 +0530

    nov-27

---
+++ Later/Java Static Nested Classes/NestedClassDemo/src/StaticNestedClassTest.java
@@ -0,0 +1,11 @@
+public class StaticNestedClassTest
+{
+
+	public static void main(String[] args)
+	{
+	
+		OuterClass.StaticNestedClass nestedObj = new OuterClass.StaticNestedClass();
+		nestedObj.innerDisplay();
+	}
+
+}
