eb3bf8e85542e5379f6fa3a69c4126c188ea6699
Ramesh <Ramesh.Babu@neustar.biz>
2015-12-03 16:47:25 +0530

    dec-3

---
+++ Later/Java Interfaces in Java Static method/InterfaceDemo/src/InterfaceTest.java
@@ -0,0 +1,14 @@
+public class InterfaceTest
+{
+
+	public static void main(String[] args)
+	{
+		/*
+		 * The static method can only be called through the interface or class
+		 * type reference, not an instance variable.
+		 */
+		InterfaceA.sayHello();
+		
+	}
+
+}
