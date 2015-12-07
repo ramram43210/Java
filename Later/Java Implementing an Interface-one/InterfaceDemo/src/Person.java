63075aa6d70e5e4f08182fec3321f2116b3373ce
Ramesh <Ramesh.Babu@neustar.biz>
2015-12-07 15:43:56 +0530

    dec 7

---
+++ Later/Java Implementing an Interface-one/InterfaceDemo/src/Person.java
@@ -0,0 +1,4 @@
+public interface Person
+{
+	void walk();
+}
---
+++ Later/Java Implementing an Interface-one/InterfaceDemo/src/Student.java
@@ -0,0 +1,11 @@
+public class Student implements Person
+{
+
+	@Override
+	public void walk()
+	{
+		System.out.println("Student walks slowly");
+
+	}
+
+}
