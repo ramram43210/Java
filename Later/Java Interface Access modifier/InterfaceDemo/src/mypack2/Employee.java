f0f791b301dd8292e1e608f5bcc9a35f0fd8f3a5
Ramesh <Ramesh.Babu@neustar.biz>
2015-12-07 15:25:54 +0530

    dec-7

---
+++ Later/Java Interface Access modifier/InterfaceDemo/src/mypack2/Employee.java
@@ -0,0 +1,15 @@
+package mypack2;
+
+import mypack1.Person;
+
+public class Employee implements Person
+{
+
+	@Override
+	public void walk()
+	{
+		System.out.println("Student walks fast");
+
+	}
+
+}
