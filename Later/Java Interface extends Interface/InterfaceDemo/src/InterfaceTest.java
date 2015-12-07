248d55c36a9a1f43f36da7590ea6cad742abd1fb
Ramesh <Ramesh.Babu@neustar.biz>
2015-12-07 19:52:27 +0530

    dec 6

---
+++ Later/Java Interface extends Interface/InterfaceDemo/src/InterfaceTest.java
@@ -0,0 +1,11 @@
+public class InterfaceTest
+{
+
+	public static void main(String[] args)
+	{
+		InterfaceB interfaceBRef = new MyClass();
+		interfaceBRef.sayHi();
+		interfaceBRef.sayBye();
+	}
+
+}
---
+++ Later/Java Interface extends Interface/InterfaceDemo/src/MyClass.java
@@ -0,0 +1,16 @@
+public class MyClass implements InterfaceB
+{
+
+	@Override
+	public void sayHi()
+	{
+		System.out.println("Hi..");
+	}
+
+	@Override
+	public void sayBye()
+	{
+		System.out.println("Bye..");
+	}
+
+}
