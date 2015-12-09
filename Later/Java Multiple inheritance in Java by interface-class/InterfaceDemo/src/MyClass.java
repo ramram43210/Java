b33e8db64ca171e31fba21829f73574569c0b612
Ramesh <Ramesh.Babu@neustar.biz>
2015-12-09 14:00:49 +0530

    dec 9

---
+++ Later/Java Multiple inheritance in Java by interface-class/InterfaceDemo/src/MyClass.java
@@ -0,0 +1,17 @@
+public class MyClass implements InterfaceA, InterfaceB
+{
+
+	@Override
+	public void sayHi()
+	{
+		System.out.println("Hi");
+
+	}
+
+	@Override
+	public void sayBye()
+	{
+		System.out.println("Bye");
+	}
+
+}
