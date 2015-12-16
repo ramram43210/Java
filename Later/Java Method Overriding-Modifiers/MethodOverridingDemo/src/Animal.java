e5cb67c7940ed0fcf3130fdc8b614a333deea0b1
Ramesh <Ramesh.Babu@neustar.biz>
2015-12-16 13:53:49 +0530

    dec 16

---
+++ Later/Java Method Overriding-Modifiers/MethodOverridingDemo/src/Animal.java
@@ -0,0 +1,7 @@
+public class Animal
+{
+	public void run()
+	{
+		System.out.println("Animal runs slowly");
+	}
+}
---
+++ Later/Java Method Overriding-Modifiers/MethodOverridingDemo/src/Tiger.java
@@ -0,0 +1,7 @@
+public class Tiger extends Animal
+{
+	public void run()
+	{
+		System.out.println("Tiger runs fast.");
+	}
+}
