0bbb93e3504024b8f9d4072e58383cb79b540eff
Ramesh <Ramesh.Babu@neustar.biz>
2015-12-17 13:44:05 +0530

    dec 17

---
+++ Later/OverloadingVsOverriding/Java Method Overloading Vs Overriding_2/MethodOverridingDemo/src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal
+{
+	public void eat()
+	{
+		System.out.println("Animal is eating...");
+	}
+}
+
+class Lion extends Animal
+{
+	public void eat()
+	{
+		System.out.println("Lion is Eating meat...");
+	}
+}
