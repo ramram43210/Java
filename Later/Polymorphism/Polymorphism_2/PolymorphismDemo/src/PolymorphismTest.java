e45a3c62305f1eca10ccd72b955679380bc88247
Ramesh <Ramesh.Babu@neustar.biz>
2015-12-21 18:39:23 +0530

    dec 21

---
+++ Later/Polymorphism/Polymorphism_2/PolymorphismDemo/src/PolymorphismTest.java
@@ -0,0 +1,14 @@
+
+public class PolymorphismTest
+{
+
+	public static void main(String[] args)
+	{
+		Animal animalRef = new Lion();
+		animalRef.eat();
+		
+		animalRef = new Dog();
+		animalRef.eat();
+	}
+
+}
