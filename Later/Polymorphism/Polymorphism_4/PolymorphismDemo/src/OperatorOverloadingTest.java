03c8f8950edb51c3d3467e93e9ec6a1a063df7b9
Ramesh <Ramesh.Babu@neustar.biz>
2015-12-22 13:55:17 +0530

    dec 22

---
+++ Later/Polymorphism/Polymorphism_4/PolymorphismDemo/src/OperatorOverloadingTest.java
@@ -0,0 +1,12 @@
+public class OperatorOverloadingTest
+{
+	public static void main(String[] args)
+	{
+		String message = "Welcome" + " John";
+		System.out.println("message : " + message);
+
+		int result = 20 + 30;
+		System.out.println("Result  : " + result);
+	}
+
+}
