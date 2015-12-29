51d9193889493640060cf3785c5f9faf0c4ebc35
Ramesh <Ramesh.Babu@neustar.biz>
2015-12-29 19:54:27 +0530

    dec 29

---
+++ Later/Super/Super_4/SuperKeywordDemo/src/SuperTest.java
@@ -0,0 +1,10 @@
+public class SuperTest
+{
+
+	public static void main(String[] args)
+	{
+		SubClass subClassObj1 = new SubClass("Peter");
+		System.out.println("--------------------------");
+		SubClass subClassObj2 = new SubClass("John",30);
+	}
+}
