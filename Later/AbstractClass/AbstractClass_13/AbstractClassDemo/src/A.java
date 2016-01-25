9e9e0a63db04c024f1a26cb0168e109ae6b7d53f
Ramesh <Ramesh.Babu@neustar.biz>
2016-01-25 14:53:18 +0530

    jan 25

---
+++ Later/AbstractClass/AbstractClass_13/AbstractClassDemo/src/A.java
@@ -0,0 +1,15 @@
+abstract class A
+{
+	abstract void hi();
+
+}
+
+class B extends A
+{
+	void hi()
+	{
+		System.out.println("hi...");
+	}
+
+}
+
---
+++ Later/AbstractClass/AbstractClass_13/AbstractClassDemo/src/AbstractClassTest.java
@@ -0,0 +1,10 @@
+public class AbstractClassTest
+{
+
+	public static void main(String[] args)
+	{
+		B b = new B();
+		b.hi();
+	}
+
+}
