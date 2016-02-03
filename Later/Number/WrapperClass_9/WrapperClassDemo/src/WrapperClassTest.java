97757c8f77de94c0cf8a4f483071b4a3eaad8570
Ramesh <Ramesh.Babu@neustar.biz>
2016-02-03 14:10:55 +0530

    feb 3

---
+++ Later/Number/WrapperClass_9/WrapperClassDemo/src/WrapperClassTest.java
@@ -0,0 +1,33 @@
+/*
+ * The Number classes include constants and useful class 
+ * methods.The MIN_VALUE and MAX_VALUE constants contain 
+ * the smallest and largest values that can be contained 
+ * by an object of that type. 
+ */
+public class WrapperClassTest
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Byte Max Value = " + Byte.MAX_VALUE);
+		System.out.println("Byte Min Value = " + Byte.MIN_VALUE);
+
+		System.out.println("Short Max Value = " + Short.MAX_VALUE);
+		System.out.println("Short Max Value = " + Short.MIN_VALUE);
+
+		System.out
+				.println("Integer Max Value = " + Integer.MAX_VALUE);
+		System.out
+				.println("Integer Max Value = " + Integer.MIN_VALUE);
+
+		System.out.println("Long Max Value = " + Long.MAX_VALUE);
+		System.out.println("Long Max Value = " + Long.MIN_VALUE);
+
+		System.out.println("Float Max Value = " + Float.MAX_VALUE);
+		System.out.println("Float Max Value = " + Float.MIN_VALUE);
+
+		System.out.println("Double Max Value = " + Double.MAX_VALUE);
+		System.out.println("Double Max Value = " + Double.MIN_VALUE);
+
+	}
+}
