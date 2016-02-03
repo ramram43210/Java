86d4d95d909b7457205f0093e397b696f2ead1c8
Ram <ramram_43210@yahoo.com>
2016-02-03 10:38:00 +0530

    feb 3

---
+++ BasicJava/AbstractClassDemo-App/AbstractClassDemo/src/AbstractClassTest.java
@@ -0,0 +1,13 @@
+
+public class AbstractClassTest
+{
+
+	public static void main(String[] args)
+	{
+		//Person personRef = new Person();
+		Person personRef = new Student();
+		personRef.walk();
+		personRef.eat();
+	}
+
+}
---
+++ BasicJava/FinalClassDemo-App/FinalClassDemo/src/Person.java
@@ -0,0 +1,18 @@
+final public class Person
+{
+
+	public int getNumberOfLegs()
+	{
+		return 2;
+	}
+
+}
+
+class Student extends Person
+{
+	public int getNumberOfLegs()
+	{
+		return 8;
+	}
+
+}
