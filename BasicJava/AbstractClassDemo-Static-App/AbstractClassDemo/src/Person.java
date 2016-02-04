b294480de73b10def08201d5cb689c688870bc59
Ram <ramram_43210@yahoo.com>
2016-02-04 10:05:56 +0530

    feb 4

---
+++ BasicJava/AbstractClassDemo-Static-App/AbstractClassDemo/src/Person.java
@@ -0,0 +1,15 @@
+public abstract class Person
+{
+
+	public static int numberOfLegs = 2;
+	
+	abstract public void walk();
+
+	public static void eat()
+	{
+		System.out.println("Eating Pizza.");
+	}
+
+}
+
+
