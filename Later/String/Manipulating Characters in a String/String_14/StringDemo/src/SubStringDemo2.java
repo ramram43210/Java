b25559946bf7303ea5c7414601e019efa5a90fcf
Ramesh <Ramesh.Babu@neustar.biz>
2016-02-11 14:17:28 +0530

    feb 11

---
+++ Later/String/Manipulating Characters in a String/String_14/StringDemo/src/SubStringDemo2.java
@@ -0,0 +1,29 @@
+public class SubStringDemo2
+{
+
+	public static void main(String[] args)
+	{
+		String str = "Welcome";
+				
+		/*
+		 * public String substring(int beginIndex)
+		 * 
+		 * Parameters: 
+		 * ----------
+		 * 
+		 * beginIndex - the beginning index,inclusive.
+		 * 
+		 * Returns: 
+		 * -------
+		 * 
+		 * Returns a string that is a substring of this
+		 * string. The substring begins with the character
+		 * at the specified index and extends to the end of
+		 * this string.
+		 */
+
+		String subString = str.substring(3);
+		System.out.println("subString = " + subString);
+
+	}
+}
