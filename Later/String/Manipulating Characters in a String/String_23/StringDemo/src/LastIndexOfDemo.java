d90ab2231f7d668c2f2ebc279363bfc6fd8282d9
Ramesh <Ramesh.Babu@neustar.biz>
2016-02-15 14:48:14 +0530

    feb 15

--- Later/String/Manipulating Characters in a String/String_23/StringDemo/src/LastIndexOfDemo.java
+++ Later/String/Manipulating Characters in a String/String_23/StringDemo/src/LastIndexOfDemo.java
@@ -1,4 +1,4 @@
-public class IndexOfDemo
+public class LastIndexOfDemo
 {
 
 	public static void main(String[] args)
@@ -6,25 +6,25 @@ public class IndexOfDemo
 		String str = "Welcome";
 
 		/*
-		 * public int indexOf(int ch, int fromIndex)
-		 *
-		 * Parameters:
+		 * public int lastIndexOf(int ch, int fromIndex)
+		 * 
+		 * Parameters: 
 		 * ----------
-		 *
+		 * 
 		 * ch - a character (Unicode code point).
 		 * fromIndex - the index to start the search from.
-		 *
-		 * Returns:
+		 * 
+		 * Returns: 
 		 * -------
-		 *
-		 * Returns the index within this string of the first
+		 * 
+		 * Returns the index within this string of the last 
 		 * occurrence of the specified character, starting
 		 * the search at the specified index. , or -1 if the
 		 * character does not occur.
 		 */
 
-		int indexPosition = str.indexOf("e", 2);
-		System.out.println("indexOf(\"e\",2) = " + indexPosition);
+		int indexPosition = str.lastIndexOf("e", 4);
+		System.out.println("lastIndexOf(\"e\",4) = " + indexPosition);
 
 	}
 }
