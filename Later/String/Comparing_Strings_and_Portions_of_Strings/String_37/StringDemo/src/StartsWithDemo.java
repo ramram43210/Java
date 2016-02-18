64319fe718072cfa0077404bf897e8c15ed6be51
Ramesh <Ramesh.Babu@neustar.biz>
2016-02-18 13:57:05 +0530

    feb18

---
+++ Later/String/Comparing_Strings_and_Portions_of_Strings/String_37/StringDemo/src/StartsWithDemo.java
@@ -0,0 +1,42 @@
+public class StartsWithDemo
+{
+	public static void main(String[] args)
+	{
+
+		/*
+		 * public int compareTo(String anotherString)
+		 * 
+		 * Parameters: 
+		 * ----------
+		 * 
+		 * anotherString - the String to be compared.
+		 * 
+		 * Returns: 
+		 * -------
+		 * 
+		 * the value 0 if the argument string is equal to
+		 * this string; 
+		 * 
+		 * a value less than 0 if this string
+		 * is lexicographically less than the string
+		 * argument; 
+		 * 
+		 * a value greater than 0 if this
+		 * string is lexicographically greater than the
+		 * string argument.
+		 */
+
+		int result = "Welcome".compareTo("Welcome");
+		System.out.println("\"Welcome\".compareTo(\"Welcome\") = "
+				+ result);
+
+		result = "Hello".compareTo("Welcome");
+		System.out.println("\"Hello\".compareTo(\"Welcome\") = "
+				+ result);
+
+		result = "Welcome".compareTo("Apple");
+		System.out.println("\"Welcome\".compareTo(\"Apple\") = "
+				+ result);
+
+	}
+}
