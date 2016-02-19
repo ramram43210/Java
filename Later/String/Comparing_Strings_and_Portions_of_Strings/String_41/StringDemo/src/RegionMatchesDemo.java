a656f431e908a089a54497387f00931007a3c6aa
Ramesh <Ramesh.Babu@neustar.biz>
2016-02-19 17:07:37 +0530

    feb19

--- Later/String/Comparing_Strings_and_Portions_of_Strings/String_41/StringDemo/src/RegionMatchesDemo.java
+++ Later/String/Comparing_Strings_and_Portions_of_Strings/String_41/StringDemo/src/RegionMatchesDemo.java
@@ -9,25 +9,26 @@ public class RegionMatchesDemo
 		String Str4 = new String("hello");
 
 		/*
-		 * public boolean regionMatches(int toffset, String
+		 * public boolean regionMatches(int toffset,
+		 						String
 		 * other, int ooffset, int len)
-		 * 
-		 * Parameters: 
+		 *
+		 * Parameters:
 		 * ----------
-		 * 
+		 *
 		 * toffset - the starting offset of the subregion in
 		 * this string.
-		 * 
+		 *
 		 * other - the string argument.
-		 * 
+		 *
 		 * ooffset - the starting offset of the subregion in
 		 * the string argument.
-		 * 
+		 *
 		 * len - the number of characters to compare.
-		 * 
-		 * Returns: 
+		 *
+		 * Returns:
 		 * -------
-		 * 
+		 *
 		 * true if the specified subregion of this string
 		 * exactly matches the specified subregion of the
 		 * string argument; false otherwise.
@@ -38,7 +39,7 @@ public class RegionMatchesDemo
 
 		System.out.print("\"Welcome to ramj2ee.com\".regionMatches(\"RAMJ2EE\") = ");
 		System.out.println(Str1.regionMatches(11, Str3, 0, 7));
-		
+
 		System.out.print("\"Welcome to ramj2ee.com\".regionMatches(\"hello\") = ");
 		System.out.println(Str1.regionMatches(11, Str4, 0, 7));
 
