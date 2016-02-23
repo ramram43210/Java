728341c6fc4f2844c823832535f9215157c75281
Ramesh <Ramesh.Babu@neustar.biz>
2016-02-23 13:20:29 +0530

    feb23

---
+++ Later/String/misc/String_48/StringDemo/src/IsEmptyDemo.java
@@ -0,0 +1,19 @@
+/*
+ * public boolean isEmpty()
+ *  
+ * Returns: 
+ * -------
+ * true if length() is 0, otherwise false
+ */
+public class IsEmptyDemo
+{
+	public static void main(String[] args)
+	{
+		String s1 = "";
+		String s2 = "welcome";
+
+		System.out.println(s1.isEmpty());//true
+		System.out.println(s2.isEmpty());//false
+
+	}
+}
