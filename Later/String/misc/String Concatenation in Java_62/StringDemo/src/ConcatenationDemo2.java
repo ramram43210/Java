0d11b474e6c04c09bb066d128fb0e062cc3b1899
Ramesh <Ramesh.Babu@neustar.biz>
2016-02-29 16:51:11 +0530

    feb29

---
+++ Later/String/misc/String Concatenation in Java_62/StringDemo/src/ConcatenationDemo2.java
@@ -0,0 +1,13 @@
+public class ConcatenationDemo2
+{
+
+	public static void main(String[] args)
+	{
+		String s1 = "Sachin ";  
+		String s2 = "Tendulkar";
+		String s3 = s1.concat(s2);
+		System.out.println(s3);// Sachin Tendulkar
+
+	}
+
+}
