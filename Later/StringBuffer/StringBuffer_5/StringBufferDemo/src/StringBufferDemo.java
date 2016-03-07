a2b5836f26659aeea27254b7bfd259f433e4cbf3
Ramesh <Ramesh.Babu@neustar.biz>
2016-03-07 14:17:02 +0530

    mar 7

---
+++ Later/StringBuffer/StringBuffer_5/StringBufferDemo/src/StringBufferDemo.java
@@ -0,0 +1,26 @@
+/*
+ * public StringBuffer insert(int offset, String str)
+ * 
+ * Inserts the string into this character sequence.
+ * 
+ * Parameters: 
+ * ---------- 
+ * offset - the offset. 
+ * str - a string.
+ * 
+ * Returns: 
+ * ------- 
+ * a reference to this object.
+ */
+public class StringBufferDemo
+{
+
+	public static void main(String[] args)
+	{
+		StringBuffer sb = new StringBuffer("Hello World");
+		String str = "My ";
+		sb.insert(6, str);
+		System.out.println(sb);
+
+	}
+}
