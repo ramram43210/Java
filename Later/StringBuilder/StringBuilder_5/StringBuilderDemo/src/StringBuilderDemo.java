6ac19048deadfcdaa93108d5d8c9afb25d244a65
Ramesh <Ramesh.Babu@neustar.biz>
2016-03-22 13:42:46 +0530

    mar22

---
+++ Later/StringBuilder/StringBuilder_5/StringBuilderDemo/src/StringBuilderDemo.java
@@ -0,0 +1,24 @@
+/*
+ * public StringBuilder insert(int offset, String str)
+ *
+ * Parameters:
+ * ----------
+ * offset - the offset.
+ * 
+ * str - a string.
+ *
+ * Returns:
+ * -------
+ * a reference to this object.
+ */
+public class StringBuilderDemo
+{
+
+	public static void main(String[] args)
+	{
+		StringBuilder sb = new StringBuilder("Hello  World");
+		String str = "My ";
+		sb.insert(6, str);
+		System.out.println(sb);
+	}
+}
