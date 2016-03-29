f10b1605fa62e454f41165be09f97c755ce73443
Ramesh <Ramesh.Babu@neustar.biz>
2016-03-29 13:20:10 +0530

    mar29

---
+++ Later/StringBuilder/StringBuilder_15/StringBuilderDemo/src/SubSequenceDemo.java
@@ -0,0 +1,31 @@
+/*
+ * public CharSequence subSequence(int start, int end)
+ * 
+ * Parameters: 
+ * ---------- 
+ * start - the start index,inclusive.
+ *  
+ * end - the end index, exclusive.
+ * 
+ * Returns: 
+ * ------- 
+ * the specified subsequence.
+ * 
+ * Throws: 
+ * ------ 
+ * IndexOutOfBoundsException - if start or end 
+ * are negative, if end is greater than length(),
+ * or if start is greater than end.
+ */
+public class SubSequenceDemo
+{
+
+	public static void main(String[] args)
+	{
+		StringBuilder sb = new StringBuilder("Welcome");
+		CharSequence charSequence = sb.subSequence(3, 6);
+		String strValue = (String) charSequence;
+		System.out.println("strValue = " + strValue);
+
+	}
+}
