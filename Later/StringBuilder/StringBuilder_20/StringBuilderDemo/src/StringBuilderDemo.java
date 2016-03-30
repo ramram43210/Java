fbfe52fe724cc2be8ef65a2d1db6d8555a57fd59
Ramesh <Ramesh.Babu@neustar.biz>
2016-03-30 15:12:47 +0530

    mar30

--- Later/StringBuilder/StringBuilder_20/StringBuilderDemo/src/StringBuilderDemo.java
+++ Later/StringBuilder/StringBuilder_20/StringBuilderDemo/src/StringBuilderDemo.java
@@ -1,15 +1,15 @@
 /*
  * public int indexOf(String str, int fromIndex)
- * 
- * Parameters: 
- * ---------- 
- * str - the substring for which to search. 
- * 
+ *
+ * Parameters:
+ * ----------
+ * str - the substring for which to search.
+ *
  * fromIndex - the index from which to start
  * the search.
- * 
- * Returns: 
- * ------- 
+ *
+ * Returns:
+ * -------
  * the index within this string of the
  * first occurrence of the specified substring, starting
  * at the specified index.
@@ -20,7 +20,7 @@ public class StringBuilderDemo
 	public static void main(String[] args)
 	{
 		String str = "com.com.com";
-		StringBuffer sb = new StringBuffer(str);
+		StringBuilder sb = new StringBuilder(str);
 		// Returns 4
 		int indexPosition = sb.indexOf("com", 2);
 		System.out.println("indexOf(\"com\",2) = " + indexPosition);
