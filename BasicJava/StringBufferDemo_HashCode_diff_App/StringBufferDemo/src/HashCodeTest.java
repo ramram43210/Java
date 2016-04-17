3b5a5dd2ac42e6c091968a777c3220b89fefb3e7
Ram <ramram_43210@yahoo.com>
2016-04-17 21:54:55 +0530

    apr17

---
+++ BasicJava/StringBufferDemo_HashCode_diff_App/StringBufferDemo/src/HashCodeTest.java
@@ -0,0 +1,42 @@
+public class HashCodeTest
+{
+
+	public static void main(String[] args)
+	{
+
+		System.out.println("Hashcode test of String:");
+		
+		/*
+		 * String is immutable(once created, can not be
+		 * modified).
+		 */
+		String str = "Welcome";
+		
+		System.out.println("Before Concat,HashCode = "
+				+ str.hashCode());
+		
+		str = str + "Peter";
+		
+		System.out.println("After Concat,HashCode  = "
+				+ str.hashCode());
+
+		System.out.println("---------------------------------");
+
+		System.out.println("Hashcode test of StringBuffer:");
+
+		/*
+		 * StringBuffer is mutable (once created, can be
+		 * modified.
+		 */
+		StringBuffer sb = new StringBuffer("Welcome");
+		
+		System.out.println("Before Concat,HashCode = "
+				+ sb.hashCode());
+		
+		sb.append("Peter");
+		
+		System.out.println("After Concat,HashCode  = "
+				+ sb.hashCode());
+
+	}
+}
