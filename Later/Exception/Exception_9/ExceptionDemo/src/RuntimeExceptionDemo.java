0290e4b97d2f9b87dc60dab6298f4fc0f45a8d1c
Ramesh <Ramesh.Babu@neustar.biz>
2016-04-21 14:17:13 +0530

    apr21

---
+++ Later/Exception/Exception_9/ExceptionDemo/src/RuntimeExceptionDemo.java
@@ -0,0 +1,12 @@
+public class RuntimeExceptionDemo
+{
+	public static void main(String[] args)
+	{
+		int a = 10;
+		int b = 0;
+
+		// Divide by zero, will lead to exception
+		int result = a / b;
+	}
+
+}
