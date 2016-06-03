9b4bc522adc2727fff4c740b1890b4794ea63aed
Ramesh <Ramesh.Babu@neustar.biz>
2016-06-03 14:59:26 +0530

    jun3

---
+++ Later/Exception/Exception_57/ExceptionDemo/src/OutOfMemoryErrorDemo.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+public class OutOfMemoryErrorDemo
+{
+
+	public static void main(String[] args)
+	{
+		String str = "Peter";
+		LinkedList<String> linkedList = new LinkedList<String>();	
+		while(true)
+		{
+			str=str+" Welcome to India";
+			System.out.println(str);
+			linkedList.add(str);			
+		}
+	}
+
+}
