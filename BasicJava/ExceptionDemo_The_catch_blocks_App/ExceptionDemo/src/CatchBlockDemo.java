fb73bede593c2029c5270e375b24fd629be2ecab
Ram <ramram_43210@yahoo.com>
2016-06-17 09:57:17 +0530

    jun17

---
+++ BasicJava/ExceptionDemo_The_catch_blocks_App/ExceptionDemo/src/CatchBlockDemo.java
@@ -0,0 +1,33 @@
+public class CatchBlockDemo
+{
+
+	public static void main(String[] args)
+	{
+
+		try
+		{
+			String str = null;
+
+			// This line will throw NullPointerException
+			System.out.println(str.length());
+
+			// This line will throw ArithmeticException
+			int a = 5 / 0;
+
+		}
+
+		catch (ArithmeticException arithmeticException)
+		{
+			arithmeticException.printStackTrace();
+		}
+
+		catch (NullPointerException nullPointerException)
+		{
+			nullPointerException.printStackTrace();
+			
+		}
+
+		System.out.println("Normal flow---");
+
+	}
+}
