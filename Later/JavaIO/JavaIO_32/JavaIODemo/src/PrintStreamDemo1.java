ae8f17565eb37a9996ed967946959dfc959f936f
Ramesh <Ramesh.Babu@neustar.biz>
2016-07-07 15:26:28 +0530

    jul7

--- Later/JavaIO/JavaIO_32/JavaIODemo/src/PrintStreamDemo1.java
+++ Later/JavaIO/JavaIO_32/JavaIODemo/src/PrintStreamDemo1.java
@@ -1,19 +1,25 @@
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
-public class PrintStreamDemo
+public class PrintStreamDemo1
 {
 
 	public static void main(String[] args) throws IOException
 
 	{
-		FileOutputStream fileOutputStream = null;
 		PrintStream printStream = null;
 		try
 		{
-			fileOutputStream = new FileOutputStream("myfile.txt");
-			printStream = new PrintStream(fileOutputStream);
+			/*
+			 * System.out
+			 * ----------
+			 * The "standard" output stream. This stream is
+			 * already open and ready to accept output data.
+			 * Typically this stream corresponds to display
+			 * output or another output destination
+			 * specified by the host environment or user.
+			 */
+			printStream = new PrintStream(System.out);
 			printStream.println(2500);
 			printStream.println("Hello Peter");
 			printStream.println(25.988);
@@ -21,10 +27,6 @@ public class PrintStreamDemo
 		}
 		finally
 		{
-			if (fileOutputStream != null)
-			{
-				fileOutputStream.close();
-			}
 			if (printStream != null)
 			{
 				printStream.close();
---
+++ Later/JavaIO/JavaIO_32/JavaIODemo/src/PrintStreamDemo3.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class PrintStreamDemo3
+{
+
+	public static void main(String[] args) throws IOException
+
+	{
+		System.out.printf("%f", 25.89);
+		System.err.println("\nSome error occured");
+	}
+}
