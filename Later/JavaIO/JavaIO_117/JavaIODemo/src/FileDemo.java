a34b6cfb16abc76207725a7d20389ecc0f11cda6
Ramesh <Ramesh.Babu@neustar.biz>
2016-09-26 18:13:59 +0530

    sep26

---
+++ Later/JavaIO/JavaIO_117/JavaIODemo/src/FileDemo.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+
+public class FileDemo
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		String filename = "myfile.txt";
+		String workingDirectory = System.getProperty("user.dir");
+
+		String filePath = workingDirectory + File.separator + filename;
+
+		System.out.println("filePath : " + filePath);
+
+		File file = new File(filePath);
+
+		if (file.createNewFile())
+		{
+			System.out.println("File is created!");
+		}
+		else
+		{
+			System.out.println("File is already existed!");
+		}
+
+	}
+
+}
