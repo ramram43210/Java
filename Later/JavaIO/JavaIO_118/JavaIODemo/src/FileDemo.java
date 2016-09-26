a58395f617c6401257eb0a52b27a3c3aeb212e33
Ramesh <Ramesh.Babu@neustar.biz>
2016-09-26 20:12:43 +0530

    sep26

---
+++ Later/JavaIO/JavaIO_118/JavaIODemo/src/FileDemo.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+public class FileDemo
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		File file = new File("D:/work/myfile.txt");
+
+		/*
+		 * Returns the time that the file denoted by this
+		 * abstract pathname was last modified.
+		 * 
+		 * Returns: A long value representing the time the
+		 * file was last modified, measured in milliseconds
+		 * since the epoch (00:00:00 GMT, January 1, 1970),
+		 * or 0L if the file does not exist or if an I/O
+		 * error occurs
+		 */
+		long lastModified = file.lastModified();
+		System.out.println("Before Format : " + lastModified);
+
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+				"MM/dd/yyyy HH:mm:ss");
+
+		String formatedStr = simpleDateFormat.format(lastModified);
+		System.out.println("After Format : " + formatedStr);
+	}
+
+}
