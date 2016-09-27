75c065d2deda7ae181e7d77204b41f0f7931aaf1
Ramesh <Ramesh.Babu@neustar.biz>
2016-09-27 19:19:25 +0530

    sep27

--- Later/JavaIO/JavaIO_121/JavaIODemo/src/ExtensionFilter.java
+++ Later/JavaIO/JavaIO_121/JavaIODemo/src/ExtensionFilter.java
@@ -1,6 +1,15 @@
 import java.io.File;
 import java.io.FilenameFilter;
 
+/*
+ * boolean accept(File dir, String name)
+ * 
+ * Parameters: 
+ * ----------
+ * 
+ * dir - the directory in which the file was found. 
+ * name - the name of the file.
+ */
 public class ExtensionFilter implements FilenameFilter
 {
 
@@ -11,6 +20,13 @@ public class ExtensionFilter implements FilenameFilter
 		this.fileExtension = fileExtension;
 	}
 
+	/*
+	 * Tests if a specified file should be included in a
+	 * file list.
+	 * 
+	 * Returns: true if and only if the name should be
+	 * included in the file list; false otherwise.
+	 */
 	@Override
 	public boolean accept(File dir, String name)
 	{
--- Later/JavaIO/JavaIO_121/JavaIODemo/src/FileDemo.java
+++ Later/JavaIO/JavaIO_121/JavaIODemo/src/FileDemo.java
@@ -2,8 +2,22 @@ import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 
+/*
+ * public String[] list(FilenameFilter filter)
+ * 
+ * Parameters: 
+ * ----------
+ * 
+ * filter - A filename filter
+ * 
+ * How to use FilenameFilter to list out all files that
+ * are end with “.txt” extension in folder “D:/work“,
+ * and then delete it.
+ */
+
 public class FileDemo
 {
+
 	private static final String DIRECTORY_NAME = "D:/work";
 
 	public static void main(String[] args) throws IOException, ParseException
@@ -18,6 +32,12 @@ public class FileDemo
 
 		ExtensionFilter extFilter = new ExtensionFilter(".txt");
 
+		/*
+		 * Returns an array of strings naming the files and
+		 * directories in the directory denoted by this
+		 * abstract pathname that satisfy the specified
+		 * filter.
+		 */
 		String[] listOfFileNames = fileDir.list(extFilter);
 
 		for (String fileName : listOfFileNames)
