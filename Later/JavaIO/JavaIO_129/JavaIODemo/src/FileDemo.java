42eeb46f6253b28dfbe427d20864fe46c0b32691
Ramesh <Ramesh.Babu@neustar.biz>
2016-10-05 14:58:03 +0530

    oct5

---
+++ Later/JavaIO/JavaIO_129/JavaIODemo/src/FileDemo.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+
+public class FileDemo
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		File file = new File("c:");
+		/*
+		 * Returns the size of the partition named by this
+		 * abstract pathname.
+		 */
+		long totalSpace = file.getTotalSpace();
+		/*
+		 * Returns the number of bytes available to this
+		 * virtual machine on the partition named by this
+		 * abstract pathname.
+		 */
+		long usableSpace = file.getUsableSpace();
+		/*
+		 * Returns the number of unallocated bytes in the
+		 * partition named by this abstract path name.
+		 */
+		long freeSpace = file.getFreeSpace();
+
+		System.out.println(" === bytes ===");
+		System.out.println("Total size : " + totalSpace + " bytes");
+		System.out.println("Space free : " + usableSpace + " bytes");
+		System.out.println("Space free : " + freeSpace + " bytes");
+
+		System.out.println(" === gigabyte ===");
+		System.out.println("Total size : " + totalSpace / 1024 / 1024 / 1024 + " GB");
+		System.out.println("Space free : " + usableSpace / 1024 / 1024 / 1024 + " GB");
+		System.out.println("Space free : " + freeSpace / 1024 / 1024 /1024 + " GB");
+	}
+
+}
