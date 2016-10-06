29ed2d1dfc0af1e782dcf6d5c40760544215b720
Ramesh <Ramesh.Babu@neustar.biz>
2016-10-06 16:56:32 +0530

    oct6

---
+++ Later/JavaIO/JavaIO_134/JavaIODemo/src/FileDemo.java
@@ -0,0 +1,10 @@
+public class FileDemo
+{
+
+	public static void main(String[] args)
+	{
+		String workingDir = System.getProperty("user.dir");
+		System.out.println("Current working directory : " + workingDir);
+	}
+
+}
