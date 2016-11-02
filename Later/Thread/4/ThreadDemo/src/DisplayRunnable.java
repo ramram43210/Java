026714043dcde10b7228b26104a6ef23b81d57ea
Ramesh <Ramesh.Babu@neustar.biz>
2016-11-02 19:20:06 +0530

    nov2

--- Later/Thread/4/ThreadDemo/src/DisplayRunnable.java
+++ Later/Thread/4/ThreadDemo/src/DisplayRunnable.java
@@ -1,7 +1,7 @@
 /*
  * If you are not extending the Thread class,your class
  * object would not be treated as a thread object.So you
- * need to explicitely create Thread class object.We are
+ * need to explicitly create Thread class object.We are
  * passing the object of your class that implements
  * Runnable so that your class run() method may execute.
  */
