6c79f1d2ca5ea7c8619480b9cd73f31c95b3278f
Ramesh <Ramesh.Babu@neustar.biz>
2016-11-11 13:32:49 +0530

    nov11

---
+++ Later/Thread/6.1/ThreadDemo/src/DisplayNumberThread.java
@@ -0,0 +1,45 @@
+/*
+ * public long getId()
+ * 
+ * Returns the identifier of this Thread. The thread ID
+ * is a positive long number generated when this thread
+ * was created. The thread ID is unique and remains
+ * unchanged during its lifetime. When a thread is
+ * terminated, this thread ID may be reused.
+ * 
+ * Returns: this thread's ID.
+ */
+
+class DisplayNumberThread extends Thread
+{
+
+	public void run()
+	{
+		System.out.println("Hi");
+	}
+
+	public static void main(String args[])
+	{
+		DisplayNumberThread dnt1 = new DisplayNumberThread();
+		DisplayNumberThread dnt2 = new DisplayNumberThread();
+
+		System.out.println("Before setting the thread name.");
+		System.out.println("dnt1 thread Name = " + dnt1.getName());
+		System.out.println("dnt2 thread Name = " + dnt2.getName());
+
+		dnt1.start();
+		dnt2.start();
+
+		/*
+		 * Changes the name of this thread to be equal to
+		 * the argument name.
+		 */
+		dnt1.setName("DisplayNumberThread1");
+		dnt2.setName("DisplayNumberThread2");
+
+		System.out.println("\nAfter setting the thread name.");
+		System.out.println("dnt1 thread Name = " + dnt1.getName());
+		System.out.println("dnt2 thread Name = " + dnt2.getName());
+
+	}
+}
