b6b1a0f6353f29480d4ac78a988f39cf1171dd22
Ramesh <Ramesh.Babu@neustar.biz>
2016-12-05 13:51:54 +0530

    dec5

---
+++ Later/Thread/46/ThreadDemo/src/RuntimeDemo.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+/*
+ * public Process exec(String command) throws
+ * 										IOException
+ * 
+ * Parameters:
+ * -----------
+ *  
+ * command - a specified system command.
+ */
+public class RuntimeDemo
+{
+	public static void main(String[] args) throws IOException
+	{
+		/*
+		 * Returns the runtime object associated with the
+		 * current Java application.
+		 */
+		Runtime runtime = Runtime.getRuntime();
+
+		/*
+		 * Executes the specified string command in a
+		 * separate process.
+		 * 
+		 * It will open a new notepad.
+		 */
+		runtime.exec("notepad");
+	}
+
+}
