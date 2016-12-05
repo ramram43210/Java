d6786d2fa34310f57d0c4eeb1ad98d0d47e6112b
Ramesh <Ramesh.Babu@neustar.biz>
2016-12-05 19:25:22 +0530

    dec5

---
+++ Later/Thread/47/RuntimeDemo/src/RuntimeDemo1.java
@@ -0,0 +1,20 @@
+public class RuntimeDemo1
+{
+	public static void main(String[] args) 
+	{
+		/*
+		 * Returns the runtime object associated with the
+		 * current Java application.
+		 */
+		Runtime runtime = Runtime.getRuntime();
+
+		/*
+		 * Returns: the maximum number of processors
+		 * available to the virtual machine; never smaller
+		 * than one
+		 */
+		int availableProcessors = runtime.availableProcessors();
+		System.out.println("availableProcessors = "+availableProcessors);
+	}
+
+}
---
+++ Later/Thread/47/RuntimeDemo/src/RuntimeDemo2.java
@@ -0,0 +1,28 @@
+public class RuntimeDemo2
+{
+	public static void main(String[] args)
+	{
+		/*
+		 * Returns the runtime object associated with the
+		 * current Java application.
+		 */
+		Runtime runtime = Runtime.getRuntime();
+		/*
+		 * Returns the total amount of memory in the Java
+		 * virtual machine.
+		 */
+		System.out.println("Total Memory: " + runtime.totalMemory());
+		/*
+		 * Returns the amount of free memory in the Java
+		 * Virtual Machine.Calling the gc method may result
+		 * in increasing the value returned by freeMemory.
+		 */
+		System.out.println("Free Memory: " + runtime.freeMemory());
+		/*
+		 * Returns the maximum amount of memory that the
+		 * Java virtual machine will attempt to use.
+		 */
+		System.out.println("max Memory: " + runtime.maxMemory());
+	}
+
+}
