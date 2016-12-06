e02f56c210e2664559c746a4e5862662a0bb1abf
Ramesh <Ramesh.Babu@neustar.biz>
2016-12-06 12:48:59 +0530

    dec6

--- Later/Thread/46/RuntimeDemo/src/RuntimeDemo.java
+++ Later/Thread/46/RuntimeDemo/src/RuntimeDemo.java
@@ -3,10 +3,10 @@ import java.io.IOException;
 /*
  * public Process exec(String command) throws
  * 										IOException
- * 
+ *
  * Parameters:
  * -----------
- *  
+ *
  * command - a specified system command.
  */
 public class RuntimeDemo
@@ -22,10 +22,10 @@ public class RuntimeDemo
 		/*
 		 * Executes the specified string command in a
 		 * separate process.
-		 * 
+		 *
 		 * It will open a new notepad.
 		 */
-		runtime.exec("notepad");
+		runtime.exec("notepad.exe");
 	}
 
 }
---
+++ Later/Thread/50/RuntimeDemo/src/RuntimeDemo.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+
+/*
+ * public Process exec(String[] cmdarray) throws
+ * 										IOException
+ * 
+ * Parameters: 
+ * -----------
+ * 
+ * cmdarray - array containing the command to call and
+ * its arguments.
+ */
+public class RuntimeDemo
+{
+
+	public static void main(String[] args) throws IOException
+	{
+
+		String[] cmdArray = new String[2];
+
+		cmdArray[0] = "notepad.exe";
+		cmdArray[1] = ".classpath";
+
+		System.out.println("Executing notepad.exe and opening example.txt");
+
+		/*
+		 * Returns the runtime object associated with the
+		 * current Java application.
+		 */
+		Runtime runtime = Runtime.getRuntime();
+
+		/*
+		 * Executes the specified command and arguments in a
+		 * separate process.
+		 * 
+		 * Returns: A new Process object for managing the
+		 * subprocess
+		 */
+		Process process = runtime.exec(cmdArray);
+
+		System.out.println("is process alive = " + process.isAlive());
+		System.out.println(cmdArray[1] + " should now open.");
+	}
+
+}
