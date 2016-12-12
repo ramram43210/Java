a651896a5796a922f7954156b23d638164895dd4
Ramesh <Ramesh.Babu@neustar.biz>
2016-12-12 13:57:42 +0530

    dec12

---
+++ Later/Thread/60/SynchronizationDemo/src/Table.java
@@ -0,0 +1,41 @@
+public class Table
+{
+	/*
+	 *  Method is not synchronized.
+	 */
+	/*public void printTable(int n)
+	{
+		for (int i = 1; i <= 5; i++)
+		{
+			System.out.println(n * i);
+			try
+			{
+				Thread.sleep(400);
+			}
+			catch (Exception e)
+			{
+				System.out.println(e);
+			}
+		}
+	}
+	*/
+	
+	/*
+	 *  Method is synchronized
+	 */
+	public synchronized void printTable(int n)
+	{
+		for (int i = 1; i <= 5; i++)
+		{
+			System.out.println(n * i);
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch (Exception e)
+			{
+				System.out.println(e);
+			}
+		}
+	}
+}
