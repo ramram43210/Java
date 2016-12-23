fc58b0f6737da9de182db94cef0c3418f9d9170a
Ramesh <Ramesh.Babu@neustar.biz>
2016-12-23 14:08:18 +0530

    dec23

---
+++ Later/Thread/79/ThreadDemo/src/StaticSynchronizationDemo.java
@@ -0,0 +1,69 @@
+class Table
+{
+
+	static void printTable(int n)
+	{
+		/*
+		 * The block synchronizes on the lock of the object
+		 * denoted by the reference .class name .class
+		 */
+		synchronized (Table.class)
+		{
+			System.out.println(Thread.currentThread().getName());
+			for (int i = 1; i <= 5; i++)
+			{
+				System.out.println(n * i);
+				try
+				{
+					Thread.sleep(400);
+				}
+				catch (Exception e)
+				{
+				}
+			}
+			System.out.println("------------------------");
+		}
+	}
+}
+
+public class StaticSynchronizationDemo
+{
+	public static void main(String t[])
+	{
+		Thread t1 = new Thread()
+		{
+			public void run()
+			{
+				Table.printTable(1);
+			}
+		};
+
+		Thread t2 = new Thread()
+		{
+			public void run()
+			{
+				Table.printTable(10);
+			}
+		};
+
+		Thread t3 = new Thread()
+		{
+			public void run()
+			{
+				Table.printTable(100);
+			}
+		};
+
+		Thread t4 = new Thread()
+		{
+			public void run()
+			{
+				Table.printTable(1000);
+			}
+		};
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+	}
+}
