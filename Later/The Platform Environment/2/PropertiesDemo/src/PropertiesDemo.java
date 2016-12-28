97ce0d8c7637cb639eb52e5bd12bcfa28eea5d50
Ramesh <Ramesh.Babu@neustar.biz>
2016-12-28 13:27:10 +0530

    dec28

---
+++ Later/The Platform Environment/2/PropertiesDemo/src/PropertiesDemo.java
@@ -0,0 +1,52 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+/*
+ * public String getProperty(String key)
+ * 
+ * Parameters: 
+ * ---------- 
+ * 
+ * key - the property key.
+ * 
+ * Returns: 
+ * -------
+ * 
+ * the value in this property list with the specified
+ * key value.
+ */
+class PropertiesDemo
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		FileReader fileReader = null;
+		try
+		{
+			fileReader = new FileReader("db.properties");
+
+			Properties p = new Properties();
+			/*
+			 * Reads a property list (key and element pairs)
+			 * from the input character stream in a simple
+			 * line-oriented format.
+			 */
+			p.load(fileReader);
+
+			/*
+			 * Searches for the property with the specified
+			 * key in this property list.
+			 */
+			System.out.println(p.getProperty("user"));
+			System.out.println(p.getProperty("password"));
+		}
+		finally
+		{
+			if (fileReader != null)
+			{
+				fileReader.close();
+			}
+		}
+	}
+}
