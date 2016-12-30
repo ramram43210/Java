600cc636f6623b5f8f0b5926dce06f623eb3cb3a
Ramesh <Ramesh.Babu@neustar.biz>
2016-12-30 13:45:45 +0530

    dec30

---
+++ Later/The Platform Environment/6/PropertiesDemo/src/PropertiesDemo.java
@@ -0,0 +1,49 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
+
+class PropertiesDemo
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		FileReader fileReader = null;
+		try
+		{
+
+			/*
+			 * Returns the system properties
+			 */
+			Properties p = System.getProperties();
+
+			/*
+			 * Returns a set of keys in this property list
+			 * where the key and its corresponding value are
+			 * strings, including distinct keys in the
+			 * default property list if a key of the same
+			 * name has not already been found from the main
+			 * properties list.
+			 */
+			Set<String> set = p.stringPropertyNames();
+
+			Iterator<String> itr = set.iterator();
+
+			while (itr.hasNext())
+			{
+				String key = (String) itr.next();
+				String value = p.getProperty(key);
+				System.out.println(key + " = " + value);
+			}
+
+		}
+		finally
+		{
+			if (fileReader != null)
+			{
+				fileReader.close();
+			}
+		}
+	}
+}
