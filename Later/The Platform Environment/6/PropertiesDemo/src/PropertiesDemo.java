c9039ba34ddc4d53c0c11fb89e93f5d428d7456f
Ramesh <Ramesh.Babu@neustar.biz>
2017-01-05 15:13:22 +0530

    jan5

--- Later/The Platform Environment/6/PropertiesDemo/src/PropertiesDemo.java
+++ Later/The Platform Environment/6/PropertiesDemo/src/PropertiesDemo.java
@@ -9,41 +9,33 @@ class PropertiesDemo
 
 	public static void main(String[] args) throws IOException
 	{
-		FileReader fileReader = null;
-		try
+
+		/*
+		 * Returns the system properties
+		 */
+		Properties p = System.getProperties();
+
+		/*
+		 * Returns a set of keys in this property list
+		 * where the key and its corresponding value are
+		 * strings, including distinct keys in the
+		 * default property list if a key of the same
+		 * name has not already been found from the main
+		 * properties list.
+		 */
+		Set<String> set = p.stringPropertyNames();
+
+		Iterator<String> itr = set.iterator();
+
+		while (itr.hasNext())
 		{
-
-			/*
-			 * Returns the system properties
-			 */
-			Properties p = System.getProperties();
-
-			/*
-			 * Returns a set of keys in this property list
-			 * where the key and its corresponding value are
-			 * strings, including distinct keys in the
-			 * default property list if a key of the same
-			 * name has not already been found from the main
-			 * properties list.
-			 */
-			Set<String> set = p.stringPropertyNames();
-
-			Iterator<String> itr = set.iterator();
-
-			while (itr.hasNext())
-			{
-				String key = (String) itr.next();
-				String value = p.getProperty(key);
-				System.out.println(key + " = " + value);
-			}
-
+			String key = (String) itr.next();
+			/*Gets the system property indicated by the specified key.
+			/
+			String value = p.getProperty(key);
+			System.out.println(key + " = " + value);
 		}
-		finally
-		{
-			if (fileReader != null)
-			{
-				fileReader.close();
-			}
-		}
+
+
 	}
 }
