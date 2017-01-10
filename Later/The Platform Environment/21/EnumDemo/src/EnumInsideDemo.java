f142035f278d7b58b133d2e8ecc1ebe84fafcb46
Ramesh <Ramesh.Babu@neustar.biz>
2017-01-10 13:08:29 +0530

    jan10

---
+++ Later/The Platform Environment/21/EnumDemo/src/EnumInsideDemo.java
@@ -0,0 +1,26 @@
+public class EnumInsideDemo
+{
+	/*
+	 * Enum is Defined inside of the class.
+	 */
+	private enum Days
+	{
+		SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+	}
+
+	public static void main(String[] args)
+	{
+		/*
+		 * The java compiler internally adds the values()
+		 * method when it creates an enum. The values()
+		 * method returns an array containing all the values
+		 * of the enum.
+		 */
+		Days[] daysArray = Days.values();
+		for (Days day : daysArray)
+		{
+			System.out.println(day);
+		}
+
+	}
+}
