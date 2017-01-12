93fbbf68bf0e4779979a8a3fc30e97b12bd840a4
Ramesh <Ramesh.Babu@neustar.biz>
2017-01-12 12:52:50 +0530

    jan12

---
+++ Later/Enum/2.1/EnumDemo/src/EnumDemo.java
@@ -0,0 +1,14 @@
+public class EnumDemo
+{
+
+	public enum Days
+	{
+		SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(Days.SUNDAY); 
+		System.out.println(Days.MONDAY);
+	}
+}
