f5d84eb029846d29ca2194e0cd4598fcbe418d3b
Ramesh <Ramesh.Babu@neustar.biz>
2017-01-13 12:43:59 +0530

    jan13

---
+++ Later/Enum/9/EnumDemo/src/EnumDemo.java
@@ -0,0 +1,24 @@
+public class EnumDemo
+{
+
+	public static void main(String[] args)
+	{
+		/*
+		 * We can obtain an array of all the possible values
+		 * of a Java enum type by calling its static
+		 * values() method.
+		 * 
+		 * All enum types get a static values() method
+		 * automatically by the Java compiler.
+		 * 
+		 * Notice the output how the names of the constants
+		 * themselves are printed out. This is one area
+		 * where Java enums are different than static final
+		 * constants.
+		 */
+		for (Level level : Level.values())
+		{
+			System.out.println(level);
+		}
+	}
+}
