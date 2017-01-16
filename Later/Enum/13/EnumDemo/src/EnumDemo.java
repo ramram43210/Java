298dc106b3c24863edb84ae5ede65c7771507d31
Ramesh <Ramesh.Babu@neustar.biz>
2017-01-16 12:54:34 +0530

    jan16

---
+++ Later/Enum/13/EnumDemo/src/EnumDemo.java
@@ -0,0 +1,13 @@
+enum UserStatus
+{
+	PENDING, ACTIVE, INACTIVE, DELETED;
+}
+
+public class EnumDemo
+{
+	public static void main(String[] args)
+	{
+		//ACTIVE
+        System.out.println(UserStatus.ACTIVE);
+	}
+}
