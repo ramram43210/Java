553183a80a352cf3db34dcc721ca1c5decb48aad
Ramesh <Ramesh.Babu@neustar.biz>
2017-01-18 13:29:35 +0530

    jan18

--- Later/Annotations/1/AnnotationDemo/src/AnnotationsDemo.java
+++ Later/Annotations/1/AnnotationDemo/src/AnnotationsDemo.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 
 public class AnnotationsDemo
 {
+
+	/*
+	*If you remove the @SuppressWarnings("unchecked") annotation, it will show warning at compile time because we are using non-generic collection.
+	*/
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String args[])
 	{
