5ec0eb9815f60f91484972fd93d76404a4542c54
Ram <ramram_43210@yahoo.com>
2017-01-31 10:56:33 +0530

    jan31

---
+++ BasicJava/Synchronization_Method_V2_Demo_App/SynchronizationDemo/src/Table.java
@@ -0,0 +1,45 @@
+public class Table
+{
+	/*
+	 *  Method is synchronized
+	 */
+	/*public synchronized void printTable(int n)
+	{
+		System.out.println("Thread Name = "
+					+ Thread.currentThread().getName());
+		for (int i = 1; i <= 5; i++)
+		{
+			System.out.println(n * i);
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch (Exception e)
+			{
+				System.out.println(e);
+			}
+		}
+	}*/
+	
+	/*
+	 *  Method is not synchronized.
+	 */
+	public void printTable(int n)
+	{
+		System.out.println("Thread Name = "
+				+ Thread.currentThread().getName());
+		for (int i = 1; i <= 5; i++)
+		{
+			System.out.println(n * i);
+			try
+			{
+				Thread.sleep(400);
+			}
+			catch (Exception e)
+			{
+				System.out.println(e);
+			}
+		}
+	}
+		
+}
