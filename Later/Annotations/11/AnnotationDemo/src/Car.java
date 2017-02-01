08f9c5e479e4101619d462fbcc8e43be64c3a41e
Ramesh <Ramesh.Babu@neustar.biz>
2017-02-01 14:34:12 +0530

    feb1

--- Later/Annotations/11/AnnotationDemo/src/Car.java
+++ Later/Annotations/11/AnnotationDemo/src/Car.java
@@ -3,27 +3,36 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /*
- * The repeating annotation has to be 
- * annotated with @Repeatable annotation.
+ * Step 1: Declare a Repeatable Annotation Type
+ * 
+ * The annotation type must be marked with the 
+ * @Repeatable meta-annotation
  */
 @Repeatable(value = Cars.class)
 @interface Manufacturer
 	{
-		String value();
+		String name();
 	};
 
-@Manufacturer("Mercedes Benz")
-@Manufacturer("Toyota")
-@Manufacturer("BMW")
-@Manufacturer("Range Rover")
-public interface Car
-{
-
-}
-
+/*
+ * Step 2: Declare the Containing Annotation Type
+ * 
+ * The containing annotation type must have a value element
+ * with an array type. The component type of the array type
+ * must be the repeatable annotation type.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @interface Cars
 	{
 		Manufacturer[] value() default
 		{};
 	}
+
+@Manufacturer(name = "Mercedes Benz")
+@Manufacturer(name = "Toyota")
+@Manufacturer(name = "BMW")
+@Manufacturer(name = "Range Rover")
+public interface Car
+{
+
+}
--- Later/Annotations/11/AnnotationDemo/src/RepeatingAnnotations.java
+++ Later/Annotations/11/AnnotationDemo/src/RepeatingAnnotations.java
@@ -1,25 +1,38 @@
-
 public class RepeatingAnnotations
 {
 
 	public static void main(String[] args)
 	{
 
-		Manufacturer[] manufacturerArray = Car.class.getAnnotationsByType(Manufacturer.class);
-		System.out.println("Number of car manufacturers is " + manufacturerArray.length);
+		/*
+		 * Retrieving Annotations using Reflection API
+		 * method
+		 */
+		Manufacturer[] manufacturerArray = Car.class
+				.getAnnotationsByType(Manufacturer.class);
+
+		System.out.println("Number of car manufacturers is "
+				+ manufacturerArray.length);
+
 		for (Manufacturer manufacturer : manufacturerArray)
 		{
-			System.out.println(manufacturer + " , value = "+manufacturer.value());
+			System.out.println(manufacturer + " , name = "
+					+ manufacturer.name());
 		}
 
 		System.out.println("\n-------Printing out Car Manufacturers--------");
 
+		/*
+		 * Retrieving Annotations using Reflection API
+		 * method
+		 */
 		Cars cars = Car.class.getAnnotation(Cars.class);
 		System.out.println(cars);
 		manufacturerArray = cars.value();
 		for (Manufacturer manufacturer : manufacturerArray)
 		{
-			System.out.println(manufacturer + " , value = "+manufacturer.value());
+			System.out.println(manufacturer + " , name = "
+					+ manufacturer.name());
 		}
 
 	}
