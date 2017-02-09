8003edf1d21676d359c3147ab29e39b4355a6c0f
Ramesh <Ramesh.Babu@neustar.biz>
2017-02-09 15:08:50 +0530

    feb9

---
+++ Later/Generics/2.1/GenericsDemo/src/GenericDemo.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class GenericDemo
+{
+	public static void main(String[] args)
+	{
+		Set<String> nameSet = new HashSet<String>();
+		nameSet.add("Peter");
+		nameSet.add("John");
+		nameSet.add("Juli");
+
+		System.out.println("----------Using for-each loop---------------");
+		/*
+		 * Using for-each loop we can loop and get each name from nameSet
+		 */
+		for (String name : nameSet)
+		{
+			System.out.println(name);
+		}
+
+		System.out.println("----------Using Iterator---------------------");
+		Iterator<String> iterator = nameSet.iterator();
+
+		while (iterator.hasNext())
+		{
+			String name = iterator.next();
+			System.out.println(name);
+		}
+
+	}
+
+}
