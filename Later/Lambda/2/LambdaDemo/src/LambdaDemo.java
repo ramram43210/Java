6c6ab5d32839207983ab7c5d3ec2b086669001fb
Ramesh <Ramesh.Babu@neustar.biz>
2017-02-24 15:20:31 +0530

    feb24

---
+++ Later/Lambda/2/LambdaDemo/src/LambdaDemo.java
@@ -0,0 +1,26 @@
+@FunctionalInterface
+interface Dog
+{
+	public void eat();
+}
+
+/**
+ * 
+ * A lambda expression can have zero or any number of arguments.
+ *
+ */
+public class LambdaDemo
+{
+
+	public static void main(String[] args)
+	{
+		/*
+		 * Java Lambda Expression Example: No Parameter.
+		 */
+		Dog dog = () -> {
+			System.out.println("eating chicken");
+		};
+		dog.eat();
+	}
+
+}
