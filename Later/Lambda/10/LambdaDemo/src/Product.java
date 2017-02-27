1991164c5dbbc9dfd60b02c0e7c1476e8d390df5
Ramesh <Ramesh.Babu@neustar.biz>
2017-02-27 19:15:52 +0530

    feb27

---
+++ Later/Lambda/10/LambdaDemo/src/Product.java
@@ -0,0 +1,45 @@
+class Product
+{
+	private int id;
+	private String name;
+	private float price;
+
+	public Product(int id, String name, float price)
+	{
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public float getPrice()
+	{
+		return price;
+	}
+
+	public void setPrice(float price)
+	{
+		this.price = price;
+	}
+
+}
