0abd103055e0fcd78d2b1d5db10a9f0940ec85d3
Ramesh <Ramesh.Babu@neustar.biz>
2017-03-02 14:07:32 +0530

    mar2

---
+++ Later/Lambda/9.4/LambdaDemo/src/Condition.java
@@ -0,0 +1,4 @@
+public interface Condition
+{
+	public boolean test(Person person);
+}
---
+++ Later/Lambda/9.4/LambdaDemo/src/NonLambdaDemo.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class NonLambdaDemo
+{
+	public static void main(String[] args)
+	{
+		List<Person> personList = Arrays.asList(
+				new Person("Carla", 33), new Person("Balu", 32),
+				new Person("Bharth", 40), new Person("Ajay", 31));
+
+		System.out.println("------------Name Starts with B---------");
+		/*
+		 * Create a method that prints all people that have name
+		 * begins with B.
+		 */
+		printNameBeginWith_B(personList,new Condition(){
+
+			@Override
+			public boolean test(Person p)
+			{
+				return p.getName().startsWith("B");
+			}
+			
+			
+		});
+		System.out.println("----------------------------------------");
+
+	}
+
+	private static void printNameBeginWith_B(List<Person> personList,Condition condition)
+	{
+		for (Person person : personList)
+		{
+			if(condition.test(person))
+			{
+				System.out.println(person);
+			}
+		}
+		
+	}
+
+}
