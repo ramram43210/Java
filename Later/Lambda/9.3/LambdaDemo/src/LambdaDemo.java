0384b8630dfdd5eb3a9f24d9f8d53f91f19e112e
Ramesh <Ramesh.Babu@neustar.biz>
2017-03-02 13:54:46 +0530

    mar2

---
+++ Later/Lambda/9.3/LambdaDemo/src/LambdaDemo.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class LambdaDemo
+{
+	public static void main(String[] args)
+	{
+		List<Person> personList = Arrays.asList(
+				new Person("Carla", 33), new Person("Balu", 32),
+				new Person("Bharth", 40), new Person("Ajay", 31));
+
+		System.out
+				.println("------------Before Sort by name---------");
+
+		printAll(personList);
+
+		System.out
+				.println("----------------------------------------");
+
+		/*
+		 * Sort list by Name
+		 */
+
+		Collections.sort(personList,
+				(p1, p2) -> p1.getName().compareTo(p2.getName()));
+
+		System.out
+				.println("\n------------After Sort by name---------");
+		/*
+		 * Create a method that prints all elements in the list.
+		 */
+
+		printAll(personList);
+
+		System.out
+				.println("----------------------------------------");
+
+	}
+
+	private static void printAll(List<Person> personList)
+	{
+		for (Person person : personList)
+		{
+			System.out.println(person);
+		}
+
+	}
+}
