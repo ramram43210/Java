d4ad2a099fcc65f862b74d875761dbed511b622b
Ramesh <Ramesh.Babu@neustar.biz>
2017-03-17 19:58:26 +0530

    mar17

--- Later/Streams/3/StreamsDemo/src/StreamsDemo.java
+++ Later/Streams/3/StreamsDemo/src/StreamsDemo.java
@@ -10,13 +10,13 @@ public class StreamsDemo
 				new Person(2, "Peter", 31),
 				new Person(3, "Steve", 25),
 				new Person(3, "Balu", 32));
-
-
-		Person person = personList.stream() //convert list to stream
-				.filter(x -> "Peter".equals(x.getName())) //we want "Peter" only
-				.findAny()  // If 'findAny' then return found
-				.orElse(null);	//If not found, return null
-
+		
+		
+		Person person = personList.stream() //convert list to stream		 		   
+				.filter(x -> "Peter".equals(x.getName()) && 31 ==x.getAge() ) // we want "Peter" with age 31 only
+				.findAny()  // If 'findAny' then return found									
+				.orElse(null);	// If not found, return null
+		
 		System.out.println(person);
 	}
 
