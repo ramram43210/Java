abcdc4708753cae8a7d4ea8924eaeb27b3e1f207
Ramesh <Ramesh.Babu@neustar.biz>
2017-03-23 14:58:43 +0530

    mar23

--- Later/Streams/9/StreamDemo/src/StreamDemo.java
+++ Later/Streams/9/StreamDemo/src/StreamDemo.java
@@ -3,8 +3,8 @@ import java.util.List;
 import java.util.LongSummaryStatistics;
 import java.util.stream.Collectors;
 /**
- * 
- * Sum by using Collectors Methods.
+ *
+ * Find Max and Min Product Price.
  *
  */
 public class StreamDemo
@@ -19,20 +19,20 @@ public class StreamDemo
 		productList.add(new Product(3, "Nokia mobile", 10000));
 		productList.add(new Product(4, "Samsung mobile", 40000));
 
-		
-		 // max() method to get max Product price   
-        Product maxPriceProduct = productList.stream()  
-                        .max((product1, product2)->   
-                        product1.getPrice() > product2.getPrice() ? 1: -1).get();  
-        
-        System.out.println("Max price product = "+maxPriceProduct);  
-        
-        // min() method to get min Product price  
-        Product minPriceProduct = productList.stream()  
-                .max((product1, product2)->   
-                product1.getPrice() < product2.getPrice() ? 1: -1).get();  
-        
-        System.out.println("Min price product = "+minPriceProduct); 
-				
+
+		 // max() method to get max Product price
+        Product maxPriceProduct = productList.stream()
+                        .max((product1, product2)->
+                        product1.getPrice() > product2.getPrice() ? 1: -1).get();
+
+        System.out.println("Max price product = "+maxPriceProduct);
+
+        // min() method to get min Product price
+        Product minPriceProduct = productList.stream()
+                .max((product1, product2)->
+                product1.getPrice() < product2.getPrice() ? 1: -1).get();
+
+        System.out.println("Min price product = "+minPriceProduct);
+
 	}
 }
