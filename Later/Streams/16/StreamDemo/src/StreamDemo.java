41adec616a60ada67399ca2f298a30faac93c6cb
Ramesh <Ramesh.Babu@neustar.biz>
2017-03-24 14:42:21 +0530

    mar24

--- Later/Streams/16/StreamDemo/src/StreamDemo.java
+++ Later/Streams/16/StreamDemo/src/StreamDemo.java
@@ -3,10 +3,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * 
+ *
  * Get the unique names in uppercase of the first 2 book authors
  * that are 30 years old or older.
- * 
+ *
  */
 public class StreamDemo
 {
@@ -26,12 +26,23 @@ public class StreamDemo
 
 		bookList.add(new Book("Angular Basics",
 				new Author("Dave", "dave@yahoo.com", 55), 3000.0));
-		
+
 		bookList.add(new Book("Jquery Basics",
 				new Author("Dave", "dave@yahoo.com", 55), 1000.0));
 
 
 		List<String> filteredBookNameList = new ArrayList<String>();
+
+		/*
+		 * From this list of books, we first need to map from books to
+		 * the book authors which gets us a stream of Authors and then
+		 * filter them to just get those authors that are 30 or over.
+		 * We’ll map the name of the Author, which returns us a
+		 * stream of Strings. We’ll map this to uppercase Strings and
+		 * make sure the elements are unique in the stream and grab
+		 * the first 2. Finally we return this as a list using toList
+		 * from java.util.streams.Collectors.
+		 */
 
 		filteredBookNameList = bookList.stream()
 				.map(book -> book.getAuthor())
