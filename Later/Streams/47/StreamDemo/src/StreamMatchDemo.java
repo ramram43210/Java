6078014a3e78b93786538f2934ddbeca30c4c13a
Ramesh <Ramesh.Babu@neustar.biz>
2017-05-02 15:17:18 +0530

    may2

---
+++ Later/Streams/47/StreamDemo/src/StreamMatchDemo.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamMatchDemo
+{
+	public static void main(String[] args)
+	{
+		List<Integer> list = Arrays.asList(5, 5, 10);
+		/*
+		 * It performs mutable reduction operation with java 8
+		 * Collector.
+		 */
+		int sum = list.stream()
+				.collect(Collectors.summingInt(i -> i));
+		System.out.println("Sum: " + sum);
+	}
+}
