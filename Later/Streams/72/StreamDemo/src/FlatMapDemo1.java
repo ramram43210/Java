dba8d49c04dfa30855cd63c99548abfbecab4cb6
Ramesh <Ramesh.Babu@neustar.biz>
2017-05-15 13:56:05 +0530

    may15

---
+++ Later/Streams/72/StreamDemo/src/FlatMapDemo1.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class FlatMapDemo1
+{
+	public static void main(String[] args)
+	{
+
+		int[][] intArray = {{1, 2, 3}, {4,5,6}};
+
+        //1. Stream<int[]>
+        Stream<int[]> arrayStream = Stream.of(intArray);
+
+        //2. Stream<int[]> -> flatMap -> IntStream
+        IntStream intStream = arrayStream.flatMapToInt(x -> Arrays.stream(x));
+
+        intStream.forEach(x -> System.out.println(x));
+	}
+
+}
