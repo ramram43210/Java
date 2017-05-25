a8cace6af6e7c0daf18d0d6dda9665fa8988c124
Ramesh <Ramesh.Babu@neustar.biz>
2017-05-25 12:43:14 +0530

    may25

--- Later/Streams/Terminal_operations/6/StreamDemo/src/StreamCountDemo.java
+++ Later/Streams/Terminal_operations/6/StreamDemo/src/StreamCountDemo.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class StreamCollectDemo
+public class StreamCountDemo
 {
 	public static void main(String[] args)
 	{
@@ -12,17 +12,17 @@ public class StreamCollectDemo
 		personList.add(new Person("Carol","carol@gmail.com",Gender.FEMALE, 23));
 		personList.add(new Person("David","david@gmail.com",	Gender.MALE, 39));
 		personList.add(new Person("Eric","eric@gmail.com", Gender.MALE, 26));
-		
+
 		/*
 		 * The count() operation simply returns total number of elements in the
 		 * stream.
-		 * 
+		 *
 		 * The following code finds how many people are male:
 		 */
 		long totalMale = personList.stream()
 				.filter(p -> p.getGender().equals(Gender.MALE)).count();
 		System.out.println("Total male = " + totalMale);
-		
+
 	}
 
 }
