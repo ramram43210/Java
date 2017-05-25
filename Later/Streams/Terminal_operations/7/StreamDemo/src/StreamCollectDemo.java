c678878310cfee620f88926584b0b6d33673e727
Ramesh <Ramesh.Babu@neustar.biz>
2017-05-25 12:46:32 +0530

    may25

---
+++ Later/Streams/Terminal_operations/7/StreamDemo/src/StreamCollectDemo.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamCollectDemo
+{
+	public static void main(String[] args)
+	{
+		List<Person> personList = new ArrayList<>();
+
+		personList.add(new Person("Alice","alice@gmail.com",	Gender.FEMALE, 26));
+		personList.add(new Person("Bob","bob@gmail.com", Gender.MALE, 32));
+		personList.add(new Person("Carol","carol@gmail.com",Gender.FEMALE, 23));
+		personList.add(new Person("David","david@gmail.com",	Gender.MALE, 39));
+		personList.add(new Person("Eric","eric@gmail.com", Gender.MALE, 26));
+		
+		/*
+		 * The forEach() operation performs an action for each element in the
+		 * stream, thus creating a side effect, such as print out information of
+		 * each female person as shown in the following code:
+		 */
+		personList.stream().filter(p -> p.getGender().equals(Gender.FEMALE))
+													.forEach(System.out::println);
+		
+	}
+
+}
