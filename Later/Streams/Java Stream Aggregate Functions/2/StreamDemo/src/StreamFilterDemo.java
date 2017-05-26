da8de3a6915e6ee6a78222f57aec48af46b47323
Ramesh <Ramesh.Babu@neustar.biz>
2017-05-26 18:48:51 +0530

    may26

---
+++ Later/Streams/Java Stream Aggregate Functions/2/StreamDemo/src/StreamFilterDemo.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamFilterDemo
+{
+	public static void main(String[] args)
+	{
+		List<Person> personList = new ArrayList<>();
+
+		personList.add(new Person("Alice", "alice@gmail.com", Gender.FEMALE, 16));
+		personList.add(new Person("Bob", "bob@gmail.com", Gender.MALE, 15));
+		personList.add(new Person("Carol", "carol@gmail.com", Gender.FEMALE, 23));
+		personList.add(new Person("David", "david@gmail.com", Gender.MALE, 19));
+		personList.add(new Person("Eric", "eric@gmail.com", Gender.MALE, 26));
+
+		/*
+		 * The map operation returns a new stream consisting of elements which
+		 * are the results of applying a given function to the elements of the
+		 * current stream. For example, converting a stream of Objects to a
+		 * stream of String or a stream of primitive numbers.
+		 * 
+		 * The Stream API provides 4 methods for the map operation:
+		 * 
+		 * map(): transforms a stream of objects of type T to a stream of
+		 * objects of type R.
+		 * 
+		 * mapToInt(): transforms a stream of objects to a stream of int
+		 * primitives.
+		 * 
+		 * mapToLong(): transforms a stream of objects to a stream of long
+		 * primitives.
+		 * 
+		 * mapToDouble(): transforms a stream of objects to a stream of double
+		 * primitives.
+		 * 
+		 */
+		Stream<String> stringStream = personList.stream()
+				.map(p -> p.getEmail());
+		stringStream.forEach(System.out::println);
+
+		System.out.println("\n----------------------\n");
+
+		personList.stream().map(p -> p.getEmail()).forEach(System.out::println);
+		
+		System.out.println("\n----------------------\n");
+		
+		IntStream intStream =personList.stream().mapToInt(p -> p.getAge());
+		intStream.forEach(age -> System.out.print(age + " - "));
+
+		System.out.println("\n----------------------\n");
+		
+		personList.stream().mapToInt(p -> p.getAge())
+				.forEach(age -> System.out.print(age + " - "));
+		
+		
+		System.out.println("\n----------------------\n");
+		
+		personList.stream().map(p -> p.getName().toUpperCase())
+				.forEach(System.out::println);
+
+	}
+
+}
