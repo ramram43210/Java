66f7aee14d9c203a15fffa6d03160db2e00e6440
Ramesh <Ramesh.Babu@neustar.biz>
2017-05-29 14:52:05 +0530

    may29

---
+++ Later/Streams/Java Stream Aggregate Functions/6/StreamDemo/src/Person.java
@@ -0,0 +1,74 @@
+enum Gender
+{
+	MALE, FEMALE
+}
+
+public class Person implements Comparable<Person>
+{
+	private String name;
+	private String email;
+	private Gender gender;
+	private int age;
+
+	public Person(String name, String email, Gender gender, int age)
+	{
+		super();
+		this.name = name;
+		this.email = email;
+		this.gender = gender;
+		this.age = age;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	public Gender getGender()
+	{
+		return gender;
+	}
+
+	public void setGender(Gender gender)
+	{
+		this.gender = gender;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+
+	public int compareTo(Person another)
+	{
+		return this.age - another.getAge();
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Person [name=" + name + ", gender=" + gender + ", age=" + age
+				+ "]";
+	}
+
+}
