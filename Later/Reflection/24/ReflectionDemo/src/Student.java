ae00be5e9aceb670b548de44c6d21735071c4015
Ramesh <Ramesh.Babu@neustar.biz>
2017-07-10 13:24:51 +0530

    jul10

---
+++ Later/Reflection/24/ReflectionDemo/src/Student.java
@@ -0,0 +1,30 @@
+class Student
+{
+	private String name;
+	private int age;
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+
+	public void displayMessage(String message)
+	{
+		System.out.println(message);
+	}
+}
