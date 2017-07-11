b0ef9572b5c88d45e1294f032276e20fc0e09ed8
Ramesh <Ramesh.Babu@neustar.biz>
2017-07-11 16:33:37 +0530

    jul11

--- Later/Reflection/26/ReflectionDemo/src/ReflectionDemo.java
+++ Later/Reflection/26/ReflectionDemo/src/ReflectionDemo.java
@@ -1,7 +1,8 @@
-import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
- *
+ * 
  * Despite the common belief it is actually possible to access private fields
  * and methods of other classes via Java Reflection. It is not even that
  * difficult. This can be very handy during unit testing.
@@ -14,30 +15,34 @@ public class ReflectionDemo
 		Student student = new Student("Peter");
 		try
 		{
-			Field privateNameField = Student.class.getDeclaredField("name");
+			Method privateGetNameMethod = Student.class
+					.getDeclaredMethod("getName", null);
+
 			/*
 			 * Set the accessible flag for this object to the indicated boolean
 			 * value. A value of true indicates that the reflected object should
 			 * suppress Java language access checking when it is used. A value
 			 * of false indicates that the reflected object should enforce Java
 			 * language access checks.
-			 *
+			 * 
 			 * First, if there is a security manager, its checkPermission method
 			 * is called with a ReflectPermission("suppressAccessChecks")
 			 * permission.
-			 *
-			 * By calling Field.setAcessible(true) you turn off the access
-			 * checks for this particular Field instance, for reflection only.
+			 * 
+			 * By calling Method.setAcessible(true) you turn off the access
+			 * checks for this particular Method instance, for reflection only.
 			 * Now you can access it even if it is private, protected or package
 			 * scope, even if the caller is not part of those scopes.
+			 * 
 			 */
-			privateNameField.setAccessible(true);
+			privateGetNameMethod.setAccessible(true);
 
-			String fieldValue = (String) privateNameField.get(student);
-			System.out.println("privateNameField value = " + fieldValue);
+			String returnValue = (String) privateGetNameMethod.invoke(student,null);
+
+			System.out.println("returnValue = " + returnValue);
 		}
-		catch (NoSuchFieldException | SecurityException
-				| IllegalArgumentException | IllegalAccessException e)
+		catch (NoSuchMethodException | InvocationTargetException
+				| IllegalAccessException | IllegalArgumentException e)
 		{
 			e.printStackTrace();
 		}
---
+++ Later/Reflection/26/ReflectionDemo/src/Student.java
@@ -0,0 +1,15 @@
+class Student
+{
+	private String name;
+
+	public Student(String name)
+	{
+		this.name = name;
+	}
+
+	private String getName()
+	{
+		return name;
+	}
+
+}
