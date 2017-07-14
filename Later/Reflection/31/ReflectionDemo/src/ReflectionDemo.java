80dab63092fd4e646f4b147d3208181246dc85a8
Ramesh <Ramesh.Babu@neustar.biz>
2017-07-14 12:37:13 +0530

    jul14

---
+++ Later/Reflection/31/ReflectionDemo/src/ReflectionDemo.java
@@ -0,0 +1,37 @@
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * How to get Generic Method Return Types
+ */
+public class ReflectionDemo
+{
+	public static void main(String[] args)
+	{
+		Method method;
+		try
+		{
+			method = Display.class.getMethod("getNameList", null);
+			Type returnType = method.getGenericReturnType();
+
+			if (returnType instanceof ParameterizedType)
+			{
+				ParameterizedType type = (ParameterizedType) returnType;
+				Type[] typeArguments = type.getActualTypeArguments();
+				for (Type typeArgument : typeArguments)
+				{
+					Class typeArgClass = (Class) typeArgument;
+					System.out.println("typeArgClass = " + typeArgClass);
+					System.out.println("typeArgClass Name = " + typeArgClass.getName());
+				}
+			}
+		}
+		catch (NoSuchMethodException | SecurityException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
