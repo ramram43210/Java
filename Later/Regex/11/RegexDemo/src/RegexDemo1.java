51823cc567a61fa87e3b3f5ef4e541825a6cbd23
Ramesh <Ramesh.Babu@neustar.biz>
2017-07-31 17:43:25 +0530

    jul31

---
+++ Later/Regex/11/RegexDemo/src/RegexDemo1.java
@@ -0,0 +1,27 @@
+import java.util.regex.Pattern;
+
+/**
+ * 
+ * \w = Any word character, short for [a-zA-Z_0-9]
+ *
+ */
+
+public class RegexDemo1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("metacharacters w");
+		
+		System.out.println(Pattern.matches("\\w", "a"));//true
+		System.out.println(Pattern.matches("\\w", "9"));//true
+		System.out.println(Pattern.matches("\\w", "_"));//true
+		System.out.println(Pattern.matches("\\w", "a9"));//false
+		
+		System.out.println("----------------------------------");
+		
+		System.out.println("metacharacters w with quantifier....");  
+		System.out.println(Pattern.matches("\\w*", "peter_99"));//true
+		System.out.println(Pattern.matches("\\w*", "peter_99@"));//false
+	}
+
+}
