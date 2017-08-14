a6a039c9492f2af7045e9b2d336acaeeaa77174d
Ramesh <Ramesh.Babu@neustar.biz>
2017-08-14 13:52:43 +0530

    aug14

--- Later/Regex/32/RegexDemo/src/RegexDemo.java
+++ Later/Regex/32/RegexDemo/src/RegexDemo.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * How to validate username with regular expression
  *
  */
@@ -12,22 +12,22 @@ public class RegexDemo
 		System.out.println("'peter34' is valid userName? = "+userNameValidator.validate("peter34"));
 		System.out.println("'peter_34' is valid userName? = "+userNameValidator.validate("peter_34"));
 		System.out.println("'peter-34' is valid userName? = "+userNameValidator.validate("peter-34"));
-		
+
 		System.out.println();
-		
+
 		/*
 		 * too short, min 3 characters
 		 */
-		System.out.println("'pk' is valid userName? = "+userNameValidator.validate("pk")); 
+		System.out.println("'pk' is valid userName? = "+userNameValidator.validate("pk"));
 		/*
 		 * “@” character is not allow
 		 */
 		System.out.println("'peter@89' is valid userName? = "+userNameValidator.validate("peter@89"));
-		
+
 		/*
 		 * too long, max characters of 15
 		 */
-		System.out.println("'peter@89' is valid userName? = "+userNameValidator.validate("peter11111323445"));
+		System.out.println("'peter11111323445' is valid userName? = "+userNameValidator.validate("peter11111323445"));
 	}
 
 }
