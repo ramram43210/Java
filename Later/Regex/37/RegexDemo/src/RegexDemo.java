3b180558d262636d9770d19c2ec85e97e0156fc9
Ramesh <Ramesh.Babu@neustar.biz>
2017-08-16 18:47:28 +0530

    aug16

---
+++ Later/Regex/37/RegexDemo/src/RegexDemo.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 
+ * How to validate Time in 12 Hours format with regular expression
+ *
+ */
+public class RegexDemo
+{
+
+	public static void main(String[] args)
+	{
+		List<String> validTimeList = Arrays.asList("2:00 am", "2:00 AM", "12:50 pm");
+
+		Time12HoursValidator time12HoursValidator = new Time12HoursValidator();
+
+		validTimeList.stream().forEach(time -> System.out.println(
+				time + " is Valid? = " + time12HoursValidator.validate(time)));
+
+		System.out.println("--------------------------------------");
+		
+		List<String> inValidTimeList = Arrays.asList("0:00 am", "23:00 AM", "12:90 pm","11:20 GM");
+		
+		inValidTimeList.stream().forEach(time -> System.out.println(
+				time + " is Valid? = " + time12HoursValidator.validate(time)));
+
+
+	}
+
+}
