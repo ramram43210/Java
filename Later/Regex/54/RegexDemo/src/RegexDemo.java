f66ff05698513580216dd5800762a4d7468a367d
Ramesh <Ramesh.Babu@neustar.biz>
2017-08-29 16:07:53 +0530

    Aug29

---
+++ Later/Regex/54/RegexDemo/src/RegexDemo.java
@@ -0,0 +1,36 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexDemo
+{
+
+	public static void main(String[] args)
+	{
+		/*
+		 * [abc] - Any of the elements in the set is matched
+		 */
+		calculateMatches("[abc]", "b");
+		calculateMatches("[abc]", "cab");
+		calculateMatches("[abc]", "cabABZ9");
+	}
+
+	private static void calculateMatches(String regex,
+			String inputText)
+	{
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(inputText);
+		int matches = 0;
+
+		/*
+		 * The find method keeps advancing through the input text and
+		 * returns true for every match, so we can use it to find the
+		 * match count as well:
+		 */
+		while (matcher.find())
+		{
+			++matches;
+		}
+		System.out.println("Number of Matches = " + matches);
+	}
+
+}
