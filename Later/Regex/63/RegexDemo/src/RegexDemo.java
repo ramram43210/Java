6733693d4a29be1bd00c82699eb3636db0ee0c2d
Ramesh <Ramesh.Babu@neustar.biz>
2017-08-30 18:18:30 +0530

    aug30

---
+++ Later/Regex/63/RegexDemo/src/RegexDemo.java
@@ -0,0 +1,28 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 
+ * Quantifiers
+ *
+ */
+public class RegexDemo
+{
+
+	public static void main(String[] args)
+	{
+		calculateMatches("a{3}", "aaa");
+		calculateMatches("a{3}", "aa");
+		calculateMatches("a{3}", "aaaaaa");
+		
+	}
+
+	private static void calculateMatches(String regex, String inputText)
+	{
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(inputText);
+		
+		System.out.println("Number of Matches = " + matcher.matches());
+	}
+
+}
