797e20d0032957357449a0b30f8cb0549671957a
Ramesh <Ramesh.Babu@neustar.biz>
2017-09-08 19:05:48 +0530

    sep8

---
+++ Later/Date/LocalTime/3/DateTimeDemo/src/LocalTimeDemo.java
@@ -0,0 +1,22 @@
+import java.time.LocalTime;
+
+public class LocalTimeDemo
+{
+
+	public static void main(String[] args)
+	{
+		LocalTime time1 = LocalTime.now();  
+	    System.out.println("time1         = "+time1);  
+	    
+	    LocalTime time2=time1.minusHours(2);
+	    System.out.println("Hours Changed = "+time2);
+	    
+	    
+	    LocalTime time3=time1.minusMinutes(30);  
+	    System.out.println("Min Changed   = "+time3);  
+	    
+	    LocalTime time4=time1.minusSeconds((10));  
+	    System.out.println("Sec Changed   = "+time4);  
+	}
+
+}
