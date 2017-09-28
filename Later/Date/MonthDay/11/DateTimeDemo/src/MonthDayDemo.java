1024899e9d309ce32a5b552a318adeb22bc6414d
Ramesh <Ramesh.Babu@neustar.biz>
2017-09-28 17:36:19 +0530

    sep28

--- Later/Date/MonthDay/11/DateTimeDemo/src/MonthDayDemo.java
+++ Later/Date/MonthDay/11/DateTimeDemo/src/MonthDayDemo.java
@@ -12,21 +12,34 @@ public class MonthDayDemo
 
 		/*
 		 * Parameters:
-		 * 
+		 *
 		 * month - the month-of-year to set in the returned month-day,
 		 * not null
-		 * 
+		 *
 		 * Returns:a MonthDay based on this month-day with the
 		 * requested month, not null
 		 */
 		monthDay = monthDay.with(Month.APRIL);
 
 		System.out.println("After month Change1 = " + monthDay);
-		
-		
+
 		monthDay = monthDay.withMonth(8);
 
 		System.out.println("After month Change2 = " + monthDay);
+
+		/*
+		 * Parameters:
+		 *
+		 * dayOfMonth - the day-of-month to set in the return
+		 * month-day, from 1 to 31
+		 *
+		 * Returns:a MonthDay based on this month-day with the
+		 * requested day, not null
+		 */
+
+		monthDay = monthDay.withDayOfMonth(30);
+
+		System.out.println("After day Change = " + monthDay);
 	}
 
 }
