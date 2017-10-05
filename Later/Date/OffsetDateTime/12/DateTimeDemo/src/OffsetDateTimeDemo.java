e0893c4575bfb6b32a0aaa3fdcb1ba52502c5498
Ramesh <Ramesh.Babu@neustar.biz>
2017-10-05 12:24:06 +0530

    oct5

---
+++ Later/Date/OffsetDateTime/12/DateTimeDemo/src/OffsetDateTimeDemo.java
@@ -0,0 +1,26 @@
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoField;
+
+public class OffsetDateTimeDemo
+{
+
+	public static void main(String[] args)
+	{
+
+		OffsetDateTime offsetDateTime = OffsetDateTime.now();
+
+		System.out.println(offsetDateTime);
+
+		/*
+		 * Parameters:
+		 * 
+		 * field - the field to get, not null
+		 * 
+		 * Returns:the value for the field
+		 */
+		int dayOfMonth = offsetDateTime.get(ChronoField.DAY_OF_MONTH);
+		System.out.println("dayOfMonth = "+dayOfMonth);
+
+	}
+
+}
