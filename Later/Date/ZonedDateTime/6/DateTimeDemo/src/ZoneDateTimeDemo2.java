deaf496448c66ce324aa034852ebb48ec2b0c824
Ramesh <Ramesh.Babu@neustar.biz>
2017-10-13 15:31:11 +0530

    oct13

---
+++ Later/Date/ZonedDateTime/6/DateTimeDemo/src/ZoneDateTimeDemo2.java
@@ -0,0 +1,28 @@
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class ZoneDateTimeDemo2
+{
+
+	public static void main(String[] args)
+	{
+
+		ZonedDateTime zonedDateTime1 = ZonedDateTime.now();
+		System.out.println(zonedDateTime1);
+		
+		/*
+		 * Parameters:
+		 * 
+		 * zone - the time-zone to change to, not null
+		 * 
+		 * Returns:a ZonedDateTime based on this date-time with the
+		 * requested zone, not null
+		 * 
+		 * If the zone ID equals 'Z', the result is ZoneOffset.UTC.
+		 */
+		ZonedDateTime zonedDateTime2 = zonedDateTime1.withZoneSameLocal(ZoneId.of("Z"));
+		System.out.println(zonedDateTime2);
+
+	}
+
+}
