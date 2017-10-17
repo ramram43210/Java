9b216ec250489e7e4a12b50f1975bcf0d12d4284
Ramesh <Ramesh.Babu@neustar.biz>
2017-10-17 18:09:41 +0530

    oct17

--- Later/Date/Clock/5/ClockDemo/src/ClockDemo.java
+++ Later/Date/Clock/5/ClockDemo/src/ClockDemo.java
@@ -8,18 +8,18 @@ public class ClockDemo
 	{
 		Clock clock1 = Clock.systemDefaultZone();
 		System.out.println("clock1 = " + clock1.instant());
-		
+
 		/*
 		 * Parameters:
-		 * 
+		 *
 		 * zone - the time-zone to use to convert the instant to
 		 * date-time, not null
-		 * 
+		 *
 		 * Returns:a clock that ticks in whole minutes using the
 		 * specified zone, not null
 		 */
 		Clock clock2 = Clock.tickMinutes(ZoneId.systemDefault());
-		System.out.println("clock2 = " + clock1.instant());
+		System.out.println("clock2 = " + clock2.instant());
 	}
 
 }
