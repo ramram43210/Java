204f22b67954112894d9a77b4bbab05e795acd2e
Ramesh <Ramesh.Babu@neustar.biz>
2017-10-26 12:35:46 +0530

    oct26

---
+++ Later/Date/Year/1/YearDemo/src/YearDemo.java
@@ -0,0 +1,18 @@
+import java.time.Year;
+
+public class YearDemo
+{
+
+	public static void main(String[] args)
+	{
+		/*
+		 * Returns:
+		 * 
+		 * the current year using the system clock and default
+		 * time-zone, not null
+		 */
+		Year y = Year.now();
+		System.out.println(y);
+	}
+
+}
