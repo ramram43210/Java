bf89723eb77ebf7dd9cc10bb15408905f4a32b62
Ramesh <Ramesh.Babu@neustar.biz>
2017-11-08 15:10:47 +0530

    nov8

---
+++ Later/Date/Period/1/PeriodDemo/src/PeriodDemo2.java
@@ -0,0 +1,12 @@
+import java.time.Period;
+
+public class PeriodDemo2
+{
+	public static void main(String[] args)
+	{
+		Period period = Period.ZERO;
+
+		System.out.println("period = "+period);
+	}
+
+}
