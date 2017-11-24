ee6204f494d36e6bf42b72aa718f14a4386521b0
Ramesh <Ramesh.Babu@neustar.biz>
2017-11-24 14:06:40 +0530

    nov24

---
+++ Later/Date/Instant/10/InstantDemo/src/InstantDemo2.java
@@ -0,0 +1,23 @@
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public class InstantDemo2
+{
+	public static void main(String[] args)
+	{
+		Instant instant = Instant.parse("2014-12-03T10:15:30.00Z");
+		System.out.println(instant);
+
+		/*
+		 * Parameters:
+		 * 
+		 * unit - the unit to truncate to, not null
+		 * 
+		 * Returns:an Instant based on this instant with the time
+		 * truncated, not null
+		 */
+		instant = instant.truncatedTo(ChronoUnit.DAYS);
+		System.out.println(instant);
+	}
+
+}
