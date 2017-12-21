476673d75469fc8d9716d44e24fdad221dc9b3b4
Ramesh <Ramesh.Babu@neustar.biz>
2017-12-21 13:05:47 +0530

    dec21

---
+++ Later/Date_classical/GregorianCalendar/8/GregorianCalendarDemo/src/GregorianCalendarDemo2.java
@@ -0,0 +1,19 @@
+import java.util.GregorianCalendar;
+
+public class GregorianCalendarDemo2
+{
+	public static void main(String[] args)
+	{
+		GregorianCalendar cal = (GregorianCalendar) GregorianCalendar
+				.getInstance();
+
+		System.out.println(cal.getTime());
+
+		/*
+		 * Returns:true (always)
+		 */
+		
+		boolean isWeekDateSupported = cal.isWeekDateSupported();
+		System.out.println(isWeekDateSupported);
+	}
+}
