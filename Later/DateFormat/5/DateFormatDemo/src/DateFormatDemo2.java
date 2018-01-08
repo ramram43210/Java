658b763de8d0762b9ecebaae7168f3547ff7ae09
Ramesh <Ramesh.Babu@neustar.biz>
2018-01-08 13:47:34 +0530

    jan8

--- Later/DateFormat/5/DateFormatDemo/src/DateFormatDemo2.java
+++ Later/DateFormat/5/DateFormatDemo/src/DateFormatDemo2.java
@@ -14,21 +14,21 @@ public class DateFormatDemo2
 
 		/*
 		 * Parameters:
-		 * 
+		 *
 		 * date - a Date to be formatted into a date/time string.
-		 * 
+		 *
 		 * toAppendTo - the string buffer for the returning date/time
 		 * string.
-		 * 
+		 *
 		 * fieldPosition - keeps track of the position of the field
 		 * within the returned string.
-		 * 
+		 *
 		 * Returns:
-		 * 
+		 *
 		 * the string buffer passed in as toAppendTo, with formatted
 		 * text appended.
 		 */
-		sb = dateFormat.format(new Date(), sb,
+		sb = dateFormat.format(date, sb,
 				new FieldPosition(DateFormat.DAY_OF_WEEK_IN_MONTH_FIELD));
 		System.out.println(sb);
 
