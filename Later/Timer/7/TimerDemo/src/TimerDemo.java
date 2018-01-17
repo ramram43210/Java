1758b20752d80b99ac5de54ff61e1da1ca69ab9e
Ramesh <Ramesh.Babu@neustar.biz>
2018-01-17 13:33:18 +0530

    jan17

---
+++ Later/Timer/7/TimerDemo/src/TimerDemo.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerDemo
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		TimerTask cleanUpTimerTask = new CleanUpTimerTask();
+		Timer timer = new Timer();
+
+		Date firstTime = new Date(System.currentTimeMillis());
+		System.out.println("firstTime = " + firstTime);
+
+		/*
+		 * Schedules the specified task for execution after the
+		 * specified delay.
+		 * 
+		 * Parameters:
+		 * 
+		 * task - task to be scheduled.
+		 * 
+		 * firstTime - First time at which task is to be executed.
+		 * 
+		 * period - time in milliseconds between successive task
+		 * executions.
+		 */
+		timer.scheduleAtFixedRate(cleanUpTimerTask, firstTime, 1000);
+		System.out.println("Timer has schedule the myTimerTask...");
+		
+		Thread.sleep(5000);
+		
+		System.out.println("Going to cancel...");
+		timer.cancel();
+		System.out.println("Canceled...");
+	}
+
+}
