f76dd0bd9c1ba44e5379a7a4b99d828a5f526099
Ramesh <Ramesh.Babu@neustar.biz>
2018-01-23 19:01:25 +0530

    jan23

---
+++ Later/Date_classical/Timestamp/3/TimestampDemo/src/TimestampDemo1.java
@@ -0,0 +1,22 @@
+import java.sql.Timestamp;
+
+public class TimestampDemo1
+{
+
+	public static void main(String[] args) throws InterruptedException
+	{
+
+		long millis = System.currentTimeMillis();
+		Timestamp timestamp = new Timestamp(millis);
+		System.out.println(timestamp);
+
+		/*
+		 * Returns:the number of milliseconds since January 1, 1970,
+		 * 00:00:00 GMT represented by this date.
+		 */
+		long milliseconds = timestamp.getTime();
+		System.out.println(milliseconds);
+
+	}
+
+}
