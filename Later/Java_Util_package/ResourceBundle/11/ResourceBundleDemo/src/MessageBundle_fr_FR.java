1aaf105a5a1007a4e351e4fa57d494f3f5559025
Ram N <ramram_43210@yahoo.com>
2018-02-07 19:16:08 +0530

    feb7

--- Later/Java_Util_package/ResourceBundle/11/ResourceBundleDemo/src/MessageBundle_fr_FR.java
+++ Later/Java_Util_package/ResourceBundle/11/ResourceBundleDemo/src/MessageBundle_fr_FR.java
@@ -2,7 +2,7 @@ import java.util.Enumeration;
 import java.util.ResourceBundle;
 import java.util.StringTokenizer;
 
-public class MessageBundle extends ResourceBundle
+public class MessageBundle_fr_FR extends ResourceBundle
 {
 	private String keys = "Hello Goodbye";
 
@@ -10,29 +10,29 @@ public class MessageBundle extends ResourceBundle
 	 * Gets an object for the given key from this resource bundle.
 	 * Returns null if this resource bundle does not contain an object
 	 * for the given key.
-	 *
+	 * 
 	 * Parameters:
-	 *
+	 * 
 	 * key - the key for the desired object
-	 *
+	 * 
 	 * Returns: the object for the given key, or null
-	 *
+	 * 
 	 */
 	@Override
 	public Object handleGetObject(String key)
 	{
 		if (key.equals("Hello"))
 		{
-			return "Hello Peter";
+			return "Bonjour Peter";
 		}
 		if (key.equals("Goodbye"))
 		{
-			return "Goodbye Steve";
+			return "Au revoir Steve";
 		}
 
 		return null;
 	}
-
+	
 	@Override
 	public Enumeration getKeys()
 	{
