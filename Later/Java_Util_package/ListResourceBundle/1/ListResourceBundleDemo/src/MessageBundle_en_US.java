b167148bbf89b2de5387517edef904f41865cd06
Ram N <ramram_43210@yahoo.com>
2018-02-08 15:18:47 +0530

    feb8

--- Later/Java_Util_package/ListResourceBundle/1/ListResourceBundleDemo/src/MessageBundle_en_US.java
+++ Later/Java_Util_package/ListResourceBundle/1/ListResourceBundleDemo/src/MessageBundle_en_US.java
@@ -1,8 +1,8 @@
 import java.util.ListResourceBundle;
 
 /**
- * 
- * Subclasses must override getContents and provide an array, where
+ *
+ * Subclasses of ListResourceBundle must override getContents and provide an array, where
  * each item in the array is a pair of objects. The first element of
  * each pair is the key, which must be a String, and the second
  * element is the value associated with that key.
@@ -16,9 +16,9 @@ public class MessageBundle_en_US extends ListResourceBundle
 	 * must be a String, and the second element is the value
 	 * associated with that key. See the class description for
 	 * details.
-	 * 
+	 *
 	 * Returns:
-	 * 
+	 *
 	 * an array of an Object array representing a key-value pair.
 	 */
 
--- Later/Java_Util_package/ListResourceBundle/1/ListResourceBundleDemo/src/MessageBundle_fr_FR.java
+++ Later/Java_Util_package/ListResourceBundle/1/ListResourceBundleDemo/src/MessageBundle_fr_FR.java
@@ -1,8 +1,8 @@
 import java.util.ListResourceBundle;
 
 /**
- * 
- * Subclasses must override getContents and provide an array, where
+ *
+ * Subclasses of ListResourceBundle must override getContents and provide an array, where
  * each item in the array is a pair of objects. The first element of
  * each pair is the key, which must be a String, and the second
  * element is the value associated with that key.
@@ -16,9 +16,9 @@ public class MessageBundle_fr_FR extends ListResourceBundle
 	 * must be a String, and the second element is the value
 	 * associated with that key. See the class description for
 	 * details.
-	 * 
+	 *
 	 * Returns:
-	 * 
+	 *
 	 * an array of an Object array representing a key-value pair.
 	 */
 
@@ -26,7 +26,7 @@ public class MessageBundle_fr_FR extends ListResourceBundle
 	protected Object[][] getContents()
 	{
 		return new Object[][] {
-				{ "hello", "Bonjour Peter" }, 
+				{ "hello", "Bonjour Peter" },
 				{ "welcome", "Bienvenue Juli" },
 		};
 	}
