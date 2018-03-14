8112c4e30512f4569e84ae1e338eae66443c7d54
Ram <ramram_43210@yahoo.com>
2018-03-14 15:37:13 +0530

    mar14

--- Later/Java_Util_package/Arrays/1_sort/5/ArraysDemo/src/ArraysDemo1.java
+++ Later/Java_Util_package/Arrays/1_sort/5/ArraysDemo/src/ArraysDemo1.java
@@ -6,21 +6,21 @@ public class ArraysDemo1
 	public static void main(String[] args)
 	{
 
-		long[] longArray = { 5, 2, 7, 3, 9 };
+		float[] floatArray = { 5.2f, 2.3f, 7.6f, 3.4f, 9.8f };
 
 		/*
 		 * Sorts the specified array into ascending numerical order.
-		 *
+		 * 
 		 * Parameters:
-		 *
+		 * 
 		 * a - the array to be sorted
-		 *
+		 * 
 		 */
-		Arrays.sort(longArray);
+		Arrays.sort(floatArray);
 
-		System.out.println("The sorted long array is:");
-		long i = 0;
-		for (long number : longArray)
+		System.out.println("The sorted float array is:");
+		float i = 0;
+		for (float number : floatArray)
 		{
 			System.out.println("Index = " + i + ", Number = " + number);
 			++i;
--- Later/Java_Util_package/Arrays/1_sort/5/ArraysDemo/src/ArraysDemo2.java
+++ Later/Java_Util_package/Arrays/1_sort/5/ArraysDemo/src/ArraysDemo2.java
@@ -6,30 +6,30 @@ public class ArraysDemo2
 	public static void main(String[] args)
 	{
 
-		long[] longArray = { 5, 2, 7, 3, 9 };
+		float[] floatArray = { 5.2f, 2.3f, 7.6f, 3.4f, 9.8f };
 
 		/*
 		 * Sorts the specified range of the array into ascending
 		 * order. The range to be sorted extends from the index
 		 * fromIndex, inclusive, to the index toIndex, exclusive. If
 		 * fromIndex == toIndex, the range to be sorted is empty.
-		 *
+		 * 
 		 * Parameters:
-		 *
+		 * 
 		 * a - the array to be sorted
-		 *
+		 * 
 		 * fromIndex - the index of the first element, inclusive, to
 		 * be sorted
-		 *
+		 * 
 		 * toIndex - the index of the last element, exclusive, to be
 		 * sorted
-		 *
+		 * 
 		 */
-		Arrays.sort(longArray, 2, 5);
+		Arrays.sort(floatArray, 2, 5);
 
-		System.out.println("The sorted long array is:");
-		long i = 0;
-		for (long number : longArray)
+		System.out.println("The sorted float array is:");
+		float i = 0;
+		for (float number : floatArray)
 		{
 			System.out.println("Index = " + i + ", Number = " + number);
 			++i;
