9aa7a6ae69be1bf824f162430a9900bb11708d10
Ram <ramram_43210@yahoo.com>
2018-03-22 12:28:19 +0530

    mar22

--- Later/Java_Util_package/Arrays/4_copyOf/8/ArraysDemo/src/ArraysDemo.java
+++ Later/Java_Util_package/Arrays/4_copyOf/8/ArraysDemo/src/ArraysDemo.java
@@ -6,40 +6,40 @@ public class ArraysDemo
 	public static void main(String[] args)
 	{
 
-		char[] charArray1 = new char[] {'a', 'v', 'g'};
+		boolean[] booleanArray1 = new boolean[] { true, false, true };
 
-		System.out.println("Printing charArray1:");
-		printArrayDetails(charArray1);
+		System.out.println("Printing booleanArray1:");
+		printArrayDetails(booleanArray1);
 
 		/*
 		 * Copies the specified array, truncating or padding with
 		 * zeros (if necessary) so the copy has the specified length.
-		 *
+		 * 
 		 * Parameters:
-		 *
+		 * 
 		 * original - the array to be copied
-		 *
+		 * 
 		 * newLength - the length of the copy to be returned
-		 *
+		 * 
 		 * Returns:
-		 *
+		 * 
 		 * a copy of the original array, truncated or padded with
 		 * zeros to obtain the specified length
 		 */
 
-		char[] charArray2 = Arrays.copyOf(charArray1, 3);
+		boolean[] booleanArray2 = Arrays.copyOf(booleanArray1, 3);
 
-		System.out.println("\nPrinting charArray2:");
-		printArrayDetails(charArray2);
+		System.out.println("\nPrinting booleanArray2:");
+		printArrayDetails(booleanArray2);
 
 	}
 
-	private static void printArrayDetails(char[] charArray)
+	private static void printArrayDetails(boolean[] booleanArray)
 	{
 		int i = 0;
-		for (char charValue : charArray)
+		for (boolean booleanValue : booleanArray)
 		{
-			System.out.println("char["+ i +"] = " + charValue);
+			System.out.println("booleanArray[" + i + "] = " + booleanValue);
 			++i;
 		}
 	}
