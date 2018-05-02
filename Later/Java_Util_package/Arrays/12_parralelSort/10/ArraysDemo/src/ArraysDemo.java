fc5f583fdad99af3c93a3d00ed6959c0e7d374fb
Ram <ramram_43210@yahoo.com>
2018-05-02 13:16:51 +0530

    may2

--- Later/Java_Util_package/Arrays/12_parralelSort/10/ArraysDemo/src/ArraysDemo.java
+++ Later/Java_Util_package/Arrays/12_parralelSort/10/ArraysDemo/src/ArraysDemo.java
@@ -1,24 +1,25 @@
 import java.util.Arrays;
 
 /**
- *
- * public static void parallelSort(float[] a,
+ * 
+ * public static void parallelSort(short[] a,
                                 int fromIndex,
                                 int toIndex)
- *
- * Sorts the specified array into ascending numerical order.
- *
+ * 
+ * Sorts the specified range of the array into ascending numerical
+ * order. The range to be sorted extends from the index fromIndex,
+ * inclusive, to the index toIndex, exclusive.
+ * 
  * The sorting algorithm is a parallel sort-merge that breaks the
  * array into sub-arrays that are themselves sorted and then merged.
- *
+ * 
  * Parameters:
- *
+ * 
  * a - the array to be sorted
- *
+ * 
  * fromIndex - the index of the first element, inclusive, to be sorted
- *
+ * 
  * toIndex - the index of the last element, exclusive, to be sorted
- *
  */
 
 public class ArraysDemo
@@ -26,9 +27,9 @@ public class ArraysDemo
 
 	public static void main(String[] args)
 	{
-		float[] floatArray = { 8.1f, 9.2f, 4.4f, 3.5f, 5.2f, 1.3f };
-		Arrays.parallelSort(floatArray, 0, 4);
-		System.out.println(Arrays.toString(floatArray));
+		short[] shortArray = { 8, 9, 4, 3, 5, 1 };
+		Arrays.parallelSort(shortArray, 0, 4);
+		System.out.println(Arrays.toString(shortArray));
 	}
 
 }
