eca8173032c364cf9e79e6bcfdbe15ff35ee8d5d
Ram <ramram_43210@yahoo.com>
2018-05-31 17:59:00 +0530

    31

--- Later/Java_Util_package/EnumSet/4/EnumSetDemo/src/EnumSetDemo.java
+++ Later/Java_Util_package/EnumSet/4/EnumSetDemo/src/EnumSetDemo.java
@@ -1,23 +1,25 @@
 import java.util.EnumSet;
 
 /**
- *
- * public static <E extends Enum<E>> EnumSet<E> of(E e1, E e2)
- *
+ * 
+ * public static <E extends Enum<E>> EnumSet<E> of(E e1, E e2, E e3)
+ * 
  * Creates an enum set initially containing the specified elements.
- *
+ * 
  * Type Parameters:
- *
+ * 
  * E - The class of the specified element and of the set
- *
+ * 
  * Parameters:
- *
+ * 
  * e1 - an element that this set is to contain initially
- *
+ * 
  * e2 - another element that this set is to contain initially
- *
+ * 
+ * e3 - another element that this set is to contain initially
+ * 
  * Returns:
- *
+ * 
  * an enum set initially containing the specified element
  *
  */
@@ -37,7 +39,8 @@ public class EnumSetDemo
 		 * elements
 		 */
 
-		EnumSet<Month> enumSet = EnumSet.of(Month.MAR, Month.JAN);
+		EnumSet<Month> enumSet = EnumSet.of(Month.MAR, Month.JAN,
+				Month.MAY);
 		System.out.println("enumSet = " + enumSet);
 	}
 
