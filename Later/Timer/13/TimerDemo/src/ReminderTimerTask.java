8a05cba17ff6e7c1cea69bd7056f59797d07f943
Ram <ramram_43210@yahoo.com>
2019-02-06 10:56:30 +0530

    1

--- Later/Timer/13/TimerDemo/src/ReminderTimerTask.java
+++
@@ -1,9 +0,0 @@
-import java.util.TimerTask;
-
-public class ReminderTimerTask extends TimerTask
-{
-	public void run()
-	{
-		System.out.println("Wake up...");
-	}
-}
--- Later/Timer/2/TimerDemo/src/CleanUpTimerTask.java
+++
@@ -1,9 +0,0 @@
-import java.util.TimerTask;
-
-public class MyTimerTask extends TimerTask
-{
-	public void run()
-	{
-		System.out.println("Clean up files...");
-	}
-}
